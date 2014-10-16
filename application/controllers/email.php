d7c9e525d99ea151b8a08f291a42a7cf314acbdb
estebanc <estebanc@121customerinsight.co.uk>
2014-10-16 16:17:31 +0100

    Rollback the last changes.

--- application/controllers/email.php
+++ application/controllers/email.php
@@ -9,7 +9,7 @@ class Email extends CI_Controller
     public function __construct()
     {
         parent::__construct();
-        user_auth_check(false);
+        user_auth_check();
 $this->_campaigns = campaign_access_dropdown();;
         $this->load->model('Records_model');
         $this->load->model('Contacts_model');
