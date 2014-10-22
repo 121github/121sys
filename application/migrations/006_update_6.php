be8dc1f68cea4c56f98f27872441570af185077a
estebanc <estebanc@121customerinsight.co.uk>
2014-10-22 09:52:42 +0100

    Changes in the 006_update file for the migrations.

--- application/migrations/006_update_6.php
+++ application/migrations/006_update_6.php
@@ -14,9 +14,9 @@ class Migration_update_6 extends CI_Migration
   PRIMARY KEY (`id`)
 ) ENGINE=innoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1");
 
-$this->db->query("INSERT INTO `121sys`.`permissions` (`permission_id`, `permission_name`, `permission_group`) VALUES (NULL, 'campaign search', 'System')");
-$this->db->query("INSERT INTO `121sys`.`permissions` (`permission_id`, `permission_name`, `permission_group`) VALUES (NULL, 'show footer', 'System')");
-$this->db->query("INSERT INTO `121sys`.`permissions` (`permission_id`, `permission_name`, `permission_group`) VALUES (NULL, 'keep_records', 'System')");
+$this->db->query("INSERT INTO `permissions` (`permission_id`, `permission_name`, `permission_group`) VALUES (NULL, 'campaign search', 'System')");
+$this->db->query("INSERT INTO `permissions` (`permission_id`, `permission_name`, `permission_group`) VALUES (NULL, 'show footer', 'System')");
+$this->db->query("INSERT INTO `permissions` (`permission_id`, `permission_name`, `permission_group`) VALUES (NULL, 'keep_records', 'System')");
 $this->db->query("ALTER TABLE `records` CHANGE `dials` `dials` INT(2) NULL DEFAULT '0'");
 $this->db->query("update records set dials = 0 where dials is null");
     }
