8c778446881f49e3c79c7499632d4d63467b9475
estebanc <estebanc@121customerinsight.co.uk>
2014-10-30 12:50:29 +0000

    Environment and session_name defined depending on the host
    Changes in the Agent Current Hours into the Management Dashboard.
    Changes in the query to get the duration working hours

--- session.php
+++ session.php
@@ -1,6 +1,31 @@
 <?php
-session_name('121sys');
+$domain = explode('121sys', $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'])[0];
+switch ($domain) {
+	case '121webhost/':
+		define('ENVIRONMENT', 'production');
+		$session_name = '121sys';
+		break;
+
+	case '121webhost/test_env/':
+		define('ENVIRONMENT', 'testing');
+		$session_name = '121sys_test';
+		break;
+
+	case '121webhost/accept_env/':
+		define('ENVIRONMENT', 'acceptance');
+		$session_name = '121sys_accept';
+		break;
+
+	default:
+		define('ENVIRONMENT', 'development');
+		$session_name = '121sys_dev';
+		break;
+}
+
+session_name($session_name);
 session_start();
+$_SESSION['session_name'] = session_name();
+$_SESSION['environment'] = ENVIRONMENT;
 
 if(isset($_GET['logout'])){
  session_destroy();
