3f3d01aa45dd96b2ebb70636b4f23a9f07b3df88
estebanc <estebanc@121customerinsight.co.uk>
2014-11-03 16:54:28 +0000

    Changes in the hours admin panel
    Adding the tables for the default time and default hours

--- assets/js/admin/hours.js
+++ assets/js/admin/hours.js
@@ -4,7 +4,7 @@ var hours = {
     	$('.daterange').daterangepicker({
             opens: "left",
             ranges: {
-            	'Next 7 Days': [moment().add('days', 6), moment()],
+            	'Next 7 Days': [moment(), moment().add('days', 6)],
             	'Tomorrow': [moment().add('days', 1), moment().add('days', 1)],
             	'Today': [moment(), moment()],
                 'Yesterday': [moment().subtract('days', 1), moment().subtract('days', 1)],
@@ -21,7 +21,7 @@ var hours = {
             $btn.find('.date-text').html(start.format('MMMM D') + ' - ' + end.format('MMMM D'));
             $btn.closest('form').find('input[name="date_from"]').val(start.format('YYYY-MM-DD'));
             $btn.closest('form').find('input[name="date_to"]').val(end.format('YYYY-MM-DD'));
-            hours.load_hours()
+            hours.load_hours();
         });
 	    $(document).on("click", '.daterange', function(e) {
 	        e.preventDefault();
@@ -32,7 +32,7 @@ var hours = {
 	        $(this).closest('form').find('input[name="campaign"]').val($(this).attr('id'));
 	        $(this).closest('ul').find('a').css("color","black");
 	        $(this).css("color","green");
-	        hours.load_hours()
+	        hours.load_hours();
 	    });
 		    $(document).on("click", ".team-filter", function(e) {
 	        e.preventDefault();
@@ -40,7 +40,7 @@ var hours = {
 			 $(this).closest('form').find('input[name="agent"]').val('');
 	        $(this).closest('ul').find('a').css("color","black");
 	        $(this).css("color","green");
-	        hours.load_hours()
+	        hours.load_hours();
 	    });
 	    $(document).on("click", ".agent-filter", function(e) {
 	        e.preventDefault();
@@ -48,7 +48,7 @@ var hours = {
 			$(this).closest('form').find('input[name="team"]').val('');
 	        $(this).closest('ul').find('a').css("color","black");
 	        $(this).css("color","green");
-	        hours.load_hours()
+	        hours.load_hours();
 	    });
 	    
         $(document).on('click', '.save-btn', function(e) {
@@ -80,7 +80,7 @@ var hours = {
             dataType: "JSON",
             data: $('.filter-form').serialize()
         }).done(function(response) {
-            $tbody = $('.hours-panel').find('tbody');
+            $tbody = $('.hours-body');
             $tbody.empty();
             $.each(response.data, function(date, hours) {
                 if (hours.length) {
@@ -118,17 +118,17 @@ var hours = {
                         var time_logged = hours + ' h ' + minutes + ' m ';
 
                         if (val.comment.length) {
-                            comment = "<a href='#'><span class='glyphicon glyphicon-comment tt pointer' data-placement='left' data-toggle='tooltip' title='" + val.comment + "'></span></a>"
+                            comment = "<a href='#'><span class='glyphicon glyphicon-comment tt pointer' data-placement='left' data-toggle='tooltip' title='" + val.comment + "'></span></a>";
                         }
                         else {
-                            comment = "<span class='glyphicon glyphicon-comment' style='opacity: 0.4; filter: alpha(opacity=40);'></span>"
+                            comment = "<span class='glyphicon glyphicon-comment' style='opacity: 0.4; filter: alpha(opacity=40);'></span>";
                         }
 
                         $tbody.append("<tr>" +
                         "<td class='hours_id hidden'>" + val.hours_id +
                         "<td class='date'>" + date +
                         "</td><td class='user_name'>" + val.user_name +
-                        "</td><td>" + duration +
+                        "</td><td><span class='hidden duration'>" + val.duration + "</span>" + duration +
                         "</td><td>" + time_logged +
                         "<td class='campaign_name'><span class='hidden campaign_id'>" + val.campaign_id + "</span>" + val.campaign_name +
                         "</td><td class='updated_name'>" + val.updated_name +
@@ -155,18 +155,20 @@ var hours = {
     	var row = $btn.closest('tr');
     	$('#edit_hours_form').find('input[name="hours_id"]').val(row.find('.hours_id').text());
         $('#edit_hours_form').find('input[name="duration"]').val(row.find('.duration').text());
-        $('#edit_hours_form').find('input[name="exception"]').val(row.find('.exception').text());
         $('#edit_hours_form').find('textarea[name="comment"]').val(row.find('.comment').text());
 		$('#edit_hours_form').find('select[name="campaign_id"]').selectpicker('val',row.find('.campaign_id').text());
-		$('#edit_hours_form').find('input[name="date"]').data('DateTimePicker').setDate(row.find('.date').text());
+		$('#edit_hours_form').find('input[name="date"]').val(row.find('.date').text());
 		
         $('.ajax-table').fadeOut(1000, function() {
             $('#edit_hours_form').fadeIn();
         });
+        
+        $('.filter-form').fadeOut(1000, function() {
+            
+        });
     },
     //save a team
     save: function($btn) {
-    	var row = $btn.closest('tr');
     	if (parseInt($('#edit_hours_form').find('input[name="duration"]').val()) - parseInt($('#edit_hours_form').find('input[name="exception"]').val()) <= 0) {
     		flashalert.danger("ERROR: hour NOT saved. The (duration - exception) can not be < 0");
     	}
@@ -193,6 +195,9 @@ var hours = {
     hide_edit_form: function() {
         $('#edit_hours_form').fadeOut(1000, function() {
             $('.ajax-table').fadeIn();
+        });
+        $('.filter-form').fadeIn(1000, function() {
+            
         });
     }
 }
