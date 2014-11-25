d6f8f9166c30705a59bf67c008d6c31b3fb0e1a7
Brad Foster <bradf@121customerinsight.co.uk>
2014-11-25 13:40:38 +0000

    changing base url

--- application/config/config.php
+++ application/config/config.php
@@ -14,8 +14,12 @@
 | path to your installation.
 |
 */
+if($_SERVER['HTTP_HOST']=="www.121system.com"){
+$config['base_url'] = 	$_SERVER['HTTP_HOST'];
+} else {
 $config['base_url'] = 'http' . ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ? 's' : '')
     .'://'.$_SERVER['HTTP_HOST'].str_replace('//','/',dirname($_SERVER['SCRIPT_NAME']).'/');
+}
 $_SESSION['base']=$config['base_url'];
 
 /*
