af4b29c0c50d804edecea1df5786555d5995b2ca
Brad Foster <bradf@121customerinsight.co.uk>
2014-11-26 14:28:42 +0000

    fixed history custom view sort

--- application/models/filter_model.php
+++ application/models/filter_model.php
@@ -506,7 +506,7 @@ class Filter_model extends CI_Model
                 "campaigns.campaign_name",
                 "fullname",
                 "outcome",
-                "date_format(r.date_updated,'%d/%m/%y %H:%i')",
+                "date_format(records.date_updated,'%d/%m/%y %H:%i')",
                 "date_format(records.nextcall,'%d/%m/%y %H:%i')",
                 "rand()"
             );
@@ -519,7 +519,7 @@ class Filter_model extends CI_Model
                 "fullname",
                 "outcome",
                 "date_format(contact,'%d/%m/%y %H:%i')",
-                "date_format(records.nextcall,'%d/%m/%y %H:%i')",
+                "date_format(history.nextcall,'%d/%m/%y %H:%i')",
                 "rand()"
             );
             $qry  = "select campaigns.campaign_name,$table.urn,fullname,$outcome_selection,date_format($table.contact,'%d/%m/%y %H:%i') date_updated, date_format(records.nextcall,'%d/%m/%y %H:%i') nextcall from $table $join_records left join contacts on records.urn = contacts.urn left join campaigns on records.campaign_id = campaigns.campaign_id left join outcomes on outcomes.outcome_id = $table.outcome_id left join progress_description on progress_description.progress_id = records.progress_id  ";
@@ -702,10 +702,11 @@ class Filter_model extends CI_Model
         
         
         $qry .= $group_by;
-		$this->firephp->log($qry);
+
         $start  = $options['start'];
         $length = $options['length'];
         $qry .= " order by CASE WHEN " . $table_columns[$options['order'][0]['column']] . " IS NULL THEN 1 ELSE 0 END," . $table_columns[$options['order'][0]['column']] . " " . $options['order'][0]['dir'];
+				$this->firephp->log($qry);
         $count = $this->db->query($qry)->num_rows();
         $qry .= " limit " . $options['length'] . " offset " . $options['start'];
         $data = $this->db->query($qry)->result_array();
