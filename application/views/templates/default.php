da5220b8256b1d110632d42b456b5009a8245e42
Brad Foster <bradf@121customerinsight.co.uk>
2014-12-02 11:56:51 +0000

    fixed calendar nav

--- application/views/templates/default.php
+++ application/views/templates/default.php
@@ -176,7 +176,7 @@ if (isset($_SESSION['current_campaign']) && in_array("show footer", $_SESSION['p
               <?php if(isset($_SESSION['current_campaign'])&&in_array("set call outcomes",$_SESSION['permissions'])){  ?>
               <li <?php if($this->uri->segment(2)=="detail"){ echo "class='active'"; } ?>><a href="<?php echo base_url(); ?>records/detail" >Start Calling</a></li>
               <?php } ?>
-              <?php if(in_array('calendar nav',$_SESSION['permissions'])||isset($_SESSION['current_campaign'])&&isset($_SESSION['campaign_features'])&&in_array('Appointment Setting',$_SESSION['campaign_features'])&&in_array("full calendar",$_SESSION['permissions'])){ ?>
+              <?php if(in_array('calendar nav',$_SESSION['permissions'])&&in_array('search campaigns',$_SESSION['permissions'])||isset($_SESSION['current_campaign'])&&isset($_SESSION['campaign_features'])&&in_array('Appointment Setting',$_SESSION['campaign_features'])&&in_array("calendar nav",$_SESSION['permissions'])){ ?>
               <li <?php if($this->uri->segment(1)=="calendar"){ echo "class='active'"; } ?>><a href="<?php echo base_url(); ?>calendar" >Calendar</a></li>
               <?php } ?>
               <?php if(isset($_SESSION['current_campaign'])&&isset($_SESSION['campaign_features'])&&in_array('Surveys',$_SESSION['campaign_features'])&&in_array("search surveys",$_SESSION['permissions'])){ ?>
