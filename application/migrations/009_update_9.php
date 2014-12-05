c9758e2b73383506b547849ec5ebc8c523db6109
Brad Foster <bradf@121customerinsight.co.uk>
2014-12-05 15:26:41 +0000

    changed migration 9

--- application/migrations/009_update_9.php
+++ application/migrations/009_update_9.php
@@ -11,14 +11,12 @@ class Migration_update_9 extends CI_Migration
 
     public function up()
     {
+        $this->db->query("ALTER TABLE uk_postcodes DROP PRIMARY KEY");
 		$this->db->query("ALTER TABLE `uk_postcodes` ADD UNIQUE(`postcode`)");
 		 $this->db->query("ALTER TABLE `uk_postcodes` ADD `postcode_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY FIRST");
-		  $this->db->query("ALTER TABLE `appointments` ADD `location_id` INT NULL DEFAULT NULL AFTER `postcode`, ADD INDEX (`postcode_id`) ");
-		   $this->db->query("ALTER TABLE `company_addresses` ADD `location_id` INT NULL DEFAULT NULL AFTER `postcode`, ADD INDEX (`postcode_id`) ");
-		   $this->db->query("ALTER TABLE `contact_addresses` ADD `location_id` INT NULL DEFAULT NULL AFTER `postcode`, ADD INDEX (`postcode_id`)  ");
-		  $this->db->query("update company_addresses left join uk_postcodes using(postcode) set company_addresses.location_id = uk_postcodes.postcode_id"); 
-		  $this->db->query("update contact_addresses left join uk_postcodes using(postcode) set contact_addresses.location_id = uk_postcodes.postcode_id"); 
-		  $this->db->query("update appointments left join uk_postcodes using(postcode) set appointments.location_id = uk_postcodes.postcode_id"); 
+		  $this->db->query("ALTER TABLE `appointments` ADD `location_id` INT NULL DEFAULT NULL AFTER `postcode`, ADD INDEX (`location_id`) ");
+		   $this->db->query("ALTER TABLE `company_addresses` ADD `location_id` INT NULL DEFAULT NULL AFTER `postcode`, ADD INDEX (`location_id`) ");
+		   $this->db->query("ALTER TABLE `contact_addresses` ADD `location_id` INT NULL DEFAULT NULL AFTER `postcode`, ADD INDEX (`location_id`)  ");
 		  
 		 $this->db->query("CREATE TABLE IF NOT EXISTS `locations` (
   `location_id` int(11) NOT NULL,
