d775fb166728f78250cfcb131c38b2d9a123b457
Brad Foster <bradf@121customerinsight.co.uk>
2014-12-05 15:55:31 +0000

    changed location cron

--- application/models/cron_model.php
+++ application/models/cron_model.php
@@ -96,6 +96,8 @@ class Cron_model extends CI_Model
                 $qry = "update company_addresses set postcode = null where company_id = '{$row['company_id']}'";
                 $this->db->query($qry);
             } else {
+				$qry = "update company_addresses set postcode = '$formatted_postcode' where company_id = '{$row['company_id']}'";
+                $this->db->query($qry);
                 $qry = "insert into locations (select postcode_id,lat,lng from uk_postcodes where postcode = '$formatted_postcode')";
                 $this->db->query($qry);
             }
@@ -116,6 +118,8 @@ class Cron_model extends CI_Model
                 $qry = "update contact_addresses set postcode = null where contact_id = '{$row['contact_id']}'";
                 $this->db->query($qry);
             } else {
+				$qry = "update contact_addresses set postcode = '$formatted_postcode' where company_id = '{$row['company_id']}'";
+                $this->db->query($qry);
                 $qry = "insert into locations (select postcode_id,lat,lng from uk_postcodes where postcode = '$formatted_postcode')";
                 $this->db->query($qry);
             }
@@ -137,6 +141,8 @@ class Cron_model extends CI_Model
                 $qry = "update appointments set postcode = null where appointment_id = '{$row['appointment_id']}'";
                 $this->db->query($qry);
             } else {
+				$qry = "update appointments set postcode = '$formatted_postcode' where company_id = '{$row['company_id']}'";
+                $this->db->query($qry);
                 $qry = "insert into locations (select postcode_id,lat,lng from uk_postcodes where postcode = '$formatted_postcode')";
                 $this->db->query($qry);
             }
