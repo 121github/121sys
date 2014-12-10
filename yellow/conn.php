7dc45b25de8b5d190437acc74301556006a8dadd
Brad Foster <bradf@121customerinsight.co.uk>
2014-12-10 16:14:03 +0000

    added yell

--- yellow/conn.php
+++ yellow/conn.php
@@ -3,12 +3,11 @@
 # Type="MYSQL"
 # HTTP="true"
 session_start();
-if (empty($_SESSION['db_name'])){ session_destroy(); };
 $hostname_salesdrive = "localhost";
 $database_salesdrive = "121sys";
 $username_salesdrive = "121sys";
 $password_salesdrive = "w5Kc8GADw49q24eP";
-$salesdrive = mysql_pconnect($hostname_salesdrive, $username_salesdrive, $password_salesdrive) or trigger_error(mysql_error(),E_USER_ERROR);
-mysql_select_db($database_salesdrive, $salesdrive);
+$salesdrive = mysql_pconnect($hostname_salesdrive, $username_salesdrive, $password_salesdrive) or die(mysql_error());
+mysql_select_db($database_salesdrive, $salesdrive) or die(mysql_error());
 
 ?>
