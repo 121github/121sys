dc8c7a90853736dc0b031d1895353de2e894d487
estebanc <estebanc@121customerinsight.co.uk>
2014-12-18 16:42:46 +0000

    Changes in the backup and restore campaign. Fixed an error in the query on the migration file 10

--- application/migrations/010_update_10.php
+++ application/migrations/010_update_10.php
@@ -25,7 +25,7 @@ class Migration_update_10 extends CI_Migration
                 `name` VARCHAR(100) NOT NULL,
                 `path` VARCHAR(255) NOT NULL,
                 `restored` TINYINT DEFAULT 0 NOT NULL,
-                `restored_date` DATETIME NULL
+                `restored_date` DATETIME NULL,
                 KEY `FK_backup_campaign_id` (`campaign_id`),
                 CONSTRAINT `FK_backup_campaign_id` FOREIGN KEY (`campaign_id`) REFERENCES `campaigns` (`campaign_id`) ON DELETE CASCADE ON UPDATE CASCADE,
                 KEY `FK_backup_user_id` (`user_id`),
