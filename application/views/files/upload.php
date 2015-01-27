b7062da16bd6159f2d3999c5c6f72871ebe2ebe3
Brad Foster <bradf@121customerinsight.co.uk>
2015-01-27 11:05:02 +0000

    fixed file save path

--- application/views/files/upload.php
+++ application/views/files/upload.php
@@ -1,5 +1,5 @@
 <h1>File Upload</h1>
-<form action="<?php echo base_url()."files/upload" ?>" id="mydropzone" class="dropzone" >
+<form action="<?php echo base_url()."files/start_upload" ?>" id="mydropzone" class="dropzone" >
 <h4>Select the upload destination</h4>
 <select class="selectpicker" name="folder"><option value="" selected>Choose the folder...</option><option value="cv" selected>CV</option></select>
 </form>
