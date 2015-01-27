94b62c8814cca53e0f7d4b197dbad8c45c31384a
estebanc <estebanc@121customerinsight.co.uk>
2015-01-27 09:37:04 +0000

    Fix errors in the 6 update file (exports_form table creation)

--- application/migrations/006_update_6.php
+++ application/migrations/006_update_6.php
@@ -18,8 +18,8 @@ class Migration_update_6 extends CI_Migration
                           description VARCHAR(255) NOT NULL,
                           header TEXT NOT NULL,
                           query TEXT NOT NULL,
-                          order by VARCHAR(25) NULL,
-                          group by VARCHAR(25) NULL,
+                          order_by VARCHAR(25) NULL,
+                          group_by VARCHAR(25) NULL,
                           date_filter VARCHAR(25),
                           campaign_filter VARCHAR(25),
                       PRIMARY KEY (export_forms_id)
