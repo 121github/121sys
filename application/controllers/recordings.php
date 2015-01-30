034d2ad01250ad4637cdc9abcb23007cb093567b
Brad Foster <bradf@121customerinsight.co.uk>
2015-01-30 16:15:57 +0000

    changing recordings

--- application/controllers/recordings.php
+++ application/controllers/recordings.php
@@ -57,6 +57,7 @@ if(!empty($transfer_number)){
 $owner = $row['owner']; 	
 $endtime = $row['endtime']; //the endtime of the call is the starttime of the transfer
 $transfer_qry = "select id,servicename,filepath,starttime,endtime,date_format(starttime,'%d/%m/%y %H:%i') calldate from calls where replace(servicename,' ','') = '$transfer_number' and owner='$owner' and starttime between '$endtime' - interval 3 second and '$endtime' + interval 3 second and calldate = date('$calltime') group by id";
+$this->firephp->log($transfer_qry);
 $transfers = $db2->query($transfer_qry)->result_array();
 foreach($transfers as $k=>$row){
 	$this->firephp->log($row);
