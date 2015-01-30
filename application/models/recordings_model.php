6bab915978c34a636f0f9efeab9980296cbc2e30
Brad Foster <bradf@121customerinsight.co.uk>
2015-01-30 15:51:43 +0000

    changing recordings

--- application/models/recordings_model.php
+++ application/models/recordings_model.php
@@ -13,12 +13,12 @@ class Recordings_model extends CI_Model
     }
 
 	public function get_numbers($urn){
-	$qry = "select replace(telephone_number,' ','') as telephone_number,description from contact_telephone left join contacts using(contact_id) where urn = '$urn'";	
+	$qry = "select replace(telephone_number,' ','') as telephone_number,contact_telephone.description from contact_telephone left join contacts using(contact_id) where urn = '$urn'";	
 	$result =  $this->db->query($qry)->result_array();	
 	foreach($result as $row){
 	$numbers[] = array("description"=>$row['description'],"number"=>$row['telephone_number']);	
 	}
-	$qry = "select replace(telephone_number,' ','') as telephone_number,description from company_telephone left join companies using(company_id) where urn = '$urn'";	
+	$qry = "select replace(telephone_number,' ','') as telephone_number,company_telephone.description from company_telephone left join companies using(company_id) where urn = '$urn'";	
 	$result =  $this->db->query($qry)->result_array();	
 		foreach($result as $row){
 	$numbers[] = array("description"=>$row['description'],"number"=>$row['telephone_number']);
