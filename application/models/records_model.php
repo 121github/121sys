d0a7f6fec2d0d3f00cf67c08b91ce3fa72e956fc
Brad Foster <bradf@121customerinsight.co.uk>
2015-02-09 11:19:56 +0000

    changed callpot order

--- application/models/records_model.php
+++ application/models/records_model.php
@@ -32,20 +32,27 @@ class Records_model extends CI_Model
 		$priority = array();
 		
 		//1st priority is call back DMS and email sents within 10 mins belonging to the user
-		$priority[] = "select urn,user_id from records left join ownership using(urn) where campaign_id = '$campaign' and record_status = 1 and parked_code is null and  progress_id is null and nextcall between now() - interval 10 MINUTE and now() + interval 10 MINUTE and (user_id = '$user_id') and outcome_id in(1,85) order by case when outcome_id = 2 then 1 else 2 end, date_updated limit 1";
-		//2nd priority is any all other missed callbacks and DMS and emails belonging to the user
-		$priority[] = "select urn,user_id from records left join ownership using(urn) where campaign_id = '$campaign' and record_status = 1 and parked_code is null and progress_id is null and nextcall<now() and outcome_id in(1,2,85) and (user_id = '$user_id') order by case when outcome_id = 2 then 1 else 2 end,nextcall,dials limit 1";
-		//3rd is any other call backs belonging to any user
+		$priority[] = "select urn,user_id from records left join ownership using(urn) where campaign_id = '$campaign' and record_status = 1 and parked_code is null and  progress_id is null and nextcall between now() - interval 10 MINUTE and now() + interval 10 MINUTE and (user_id = '$user_id') and outcome_id in(2,85) order by case when outcome_id = 2 then 1 else 2 end, date_updated limit 1";
+		//next priority is any all other DMS and emails belonging to the user
+		$priority[] = "select urn,user_id from records left join ownership using(urn) where campaign_id = '$campaign' and record_status = 1 and parked_code is null and progress_id is null and nextcall<now() and outcome_id in(2,85) and (user_id = '$user_id') order by case when outcome_id = 2 then 1 else 2 end,nextcall,dials limit 1";
+		//next priority is virgin and assigend to the user
+		$priority[] = "select urn,user_id from records left join ownership using(urn) where campaign_id = '$campaign' and record_status = 1 and parked_code is null and progress_id is null and (outcome_id is null) and (user_id = '$user_id') order by date_updated,dials limit 1";
 		if(in_array("search unassigned",$_SESSION['permissions'])){
+		//next priority is virgin and unassigned
+		$priority[] = "select urn,user_id from records left join ownership using(urn) where campaign_id = '$campaign' and record_status = 1 and parked_code is null and progress_id is null and outcome_id is null and user_id is null order by date_updated,dials limit 1";
+		}
+		//next priority is lapsed callbacks	beloning to the user
+		$priority[] = "select urn,user_id from records left join ownership using(urn) where campaign_id = '$campaign' and record_status = 1 and parked_code is null and progress_id is null and nextcall<now() and outcome_id in(1,2,85) and (user_id = '$user_id') order by case when outcome_id = 2 then 1 else 2 end,nextcall,date_updated,dials limit 1";
+		//next priority is lapsed callbacks	for anyone
+		if(in_array("search unassigned",$_SESSION['permissions'])){		
 		$priority[] = "select urn,user_id from records left join ownership using(urn) where campaign_id = '$campaign' and record_status = 1 and parked_code is null and progress_id is null and nextcall<now() and outcome_id in(1,2,85) and user_id is null order by case when outcome_id = 2 then 1 else 2 end,date_updated,dials limit 1";
 		}
-		//4th priority is virgin, then any other record with a nextcall date in order of lowest dials (current user)
-		$priority[] = "select urn,user_id from records left join ownership using(urn) where campaign_id = '$campaign' and record_status = 1 and parked_code is null and progress_id is null and (outcome_id is null or nextcall<now()) and (user_id = '$user_id') order by case when outcome_id is null then 1 else 2 end,date_updated,dials limit 1";	
-		//5th priority is virgin, then any other record with a nextcall date in order of lowest dials (any user)
+		//next priority is any other record with a nextcall date in order of lowest dials (current user)
+		$priority[] = "select urn,user_id from records left join ownership using(urn) where campaign_id = '$campaign' and record_status = 1 and parked_code is null and progress_id is null and nextcall<now() and (user_id = '$user_id') order by date_updated,dials limit 1";	
+		//next any other record with a nextcall date in order of lowest dials (any user)
 		if(in_array("search unassigned",$_SESSION['permissions'])){
-		$priority[] = "select urn,user_id from records left join ownership using(urn) where campaign_id = '$campaign' and record_status = 1 and parked_code is null and progress_id is null and (outcome_id is null or nextcall<now()) and user_id is null order by case when outcome_id is null then 1 else 2 end,date_updated,dials limit 1";
-		}
-		
+		$priority[] = "select urn,user_id from records left join ownership using(urn) where campaign_id = '$campaign' and record_status = 1 and parked_code is null and progress_id is null and nextcall<now() and user_id is null order by date_updated,dials limit 1";
+		}		
 		
 		foreach($priority as $k=>$qry){
 		$query = $this->db->query($qry);
