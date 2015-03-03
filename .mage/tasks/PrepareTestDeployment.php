331eb1d73b44e4b2390eb65e9a7851c93d52d932
estebanc <estebanc@121customerinsight.co.uk>
2015-03-03 11:33:39 +0000

    Adding mage config files for the test environment #121SYS-11

---
+++ .mage/tasks/PrepareTestDeployment.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Task;
+
+use Mage\Task\AbstractTask;
+
+class PrepareTestDeployment extends AbstractTask
+{
+    public function getName()
+    {
+        return 'Preparing the deployment on the test environment';
+    }
+
+    public function run()
+    {
+        $commandList = array(
+            'mv application/config/database.php.test application/config/database.php',
+            'rm -rf application/config/database.php.*',
+            'setfacl -R -m u:www-data:rwx -m u:\`whoami\`:rwx datafiles',
+            'setfacl -dR -m u:www-data:rwx -m u:\`whoami\`:rwx datafiles',
+            'chmod -R 777 importcsv.sh'
+        );
+
+        $command = implode(" && ", $commandList);
+
+        $result = $this->runCommandRemote($command);
+
+        return $result;
+    }
+}
