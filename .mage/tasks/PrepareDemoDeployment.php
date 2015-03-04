be0871ba1e5358d3fe3e1803146efff460071d75
estebanc <estebanc@121customerinsight.co.uk>
2015-03-04 16:16:40 +0000

    Adding the demo config files and the demo deployment mage config file #121SYS-11

---
+++ .mage/tasks/PrepareDemoDeployment.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Task;
+
+use Mage\Task\AbstractTask;
+
+class PrepareDemoDeployment extends AbstractTask
+{
+    public function getName()
+    {
+        return 'Preparing the deployment on the production environment';
+    }
+
+    public function run()
+    {
+        $commandList = array(
+            'mv application/config/database.php.demo application/config/database.php',
+            'rm -rf application/config/database.php.*',
+            'setfacl -R -m u:www-data:rwx -m u:\`whoami\`:rwx datafiles',
+            'setfacl -dR -m u:www-data:rwx -m u:\`whoami\`:rwx datafiles',
+            'chmod -R 777 importcsv.sh'
+        );
+
+        $command = implode(" && ", $commandList);
+
+        $result = $this->runCommandRemote($command);
+
+        return $result;
+    }
+}
