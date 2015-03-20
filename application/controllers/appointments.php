90b606105b9333a5ebb221302ac1c2785de2bccd
Brad Foster <bradf@121customerinsight.co.uk>
2015-03-20 10:23:07 +0000

    appointments dash nav

--- application/controllers/appointments.php
+++ application/controllers/appointments.php
@@ -24,7 +24,7 @@ public function index(){
             'campaign_access' => $this->_campaigns,
             'pageId' => 'System appointment',
             'title' => 'Appointments',
-			'page'=> "appointments",
+			'page'=> array('dashboard'=>'appointments'),
 			            'css' => array(
                 'dashboard.css',
                 'plugins/morris/morris-0.4.3.min.css',
