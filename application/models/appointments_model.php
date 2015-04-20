18bb91d78e3daf1486f6bfffc37aa544b6caf2c6
estebanc <estebanc@121customerinsight.co.uk>
2015-04-20 16:27:37 +0100

    Block out days for agents: Check the block days when we try to insert/update an appointment #121SYS-40 fixed

--- application/models/appointments_model.php
+++ application/models/appointments_model.php
@@ -3,20 +3,23 @@
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
-class Appointments_model extends CI_Model {
+class Appointments_model extends CI_Model
+{
 
-    function __construct() {
+    function __construct()
+    {
         parent::__construct();
     }
-  
-public function appointment_data($count=false,$options=false){
-	      $table_columns = array(
+
+    public function appointment_data($count = false, $options = false)
+    {
+        $table_columns = array(
             "start",
             "com.name",
             "title",
             "u.name",
-             "date_format(a.`date_added`,'%d/%m/%y')",
-			 "postcode"
+            "date_format(a.`date_added`,'%d/%m/%y')",
+            "postcode"
         );
         $order_columns = array(
             "start",
@@ -24,20 +27,20 @@ public function appointment_data($count=false,$options=false){
             "title",
             "u.name",
             "a.date_added",
-			"postcode"
+            "postcode"
         );
-	
-	$qry = "select date_format(`start`,'%d/%m/%y %H:%i') start,com.name,title,u.name attendee,date_format(a.`date_added`,'%d/%m/%y') date_added,postcode,appointment_id from appointments a left join appointment_attendees aa using(appointment_id) left join users u on u.user_id = aa.user_id left join records using(urn) left join companies com using(urn) ";
-	$where = $this->get_where($options, $table_columns);
-		$qry .= $where;
-		$qry .= " group by appointment_id";
-	if($count){
-		
-	return $this->db->query($qry)->num_rows();
-	}
-	
 
-	 $start  = $options['start'];
+        $qry = "select date_format(`start`,'%d/%m/%y %H:%i') start,com.name,title,u.name attendee,date_format(a.`date_added`,'%d/%m/%y') date_added,postcode,appointment_id from appointments a left join appointment_attendees aa using(appointment_id) left join users u on u.user_id = aa.user_id left join records using(urn) left join companies com using(urn) ";
+        $where = $this->get_where($options, $table_columns);
+        $qry .= $where;
+        $qry .= " group by appointment_id";
+        if ($count) {
+
+            return $this->db->query($qry)->num_rows();
+        }
+
+
+        $start = $options['start'];
         $length = $options['length'];
         if (isset($_SESSION['appointment_table']['order']) && $options['draw'] == "1") {
             $order = $_SESSION['appointment_table']['order'];
@@ -46,18 +49,18 @@ public function appointment_data($count=false,$options=false){
             unset($_SESSION['appointment_table']['order']);
             unset($_SESSION['appointment_table']['values']['order']);
         }
-        
+
         $qry .= $order;
         $qry .= "  limit $start,$length";
-	$result = $this->db->query($qry)->result_array();
-	return $result;
-}
+        $result = $this->db->query($qry)->result_array();
+        return $result;
+    }
 
-private function get_where($options, $table_columns)
+    private function get_where($options, $table_columns)
     {
         //the default condition in ever search query to stop people viewing campaigns they arent supposed to!
         $where = " where campaign_id in({$_SESSION['campaign_access']['list']}) ";
-        
+
         //check the tabel header filter
         foreach ($options['columns'] as $k => $v) {
             //if the value is not empty we add it to the where clause
@@ -67,14 +70,27 @@ private function get_where($options, $table_columns)
         }
         return $where;
     }
-public function appointment_modal($id){
-	$qry = "select * from appointments a left join users u on u.user_id = a.created_by where appointment_id = ".intval($id)." group by appointment_id";	
-	return $this->db->query($qry)->row_array();
-}
 
-public function appointment_attendees($id){
-	$qry = "select name from appointment_attendees left join users using(user_id) where appointment_id = ".intval($id);	
-	return $this->db->query($qry)->result_array();
-}
+    public function appointment_modal($id)
+    {
+        $qry = "select * from appointments a left join users u on u.user_id = a.created_by where appointment_id = " . intval($id) . " group by appointment_id";
+        return $this->db->query($qry)->row_array();
+    }
+
+    public function appointment_attendees($id)
+    {
+        $qry = "select name from appointment_attendees left join users using(user_id) where appointment_id = " . intval($id);
+        return $this->db->query($qry)->result_array();
+    }
+
+    public function checkDayBlocked($attendee_id, $startDate, $endDate) {
+
+        $qry = "select * from appointment_rules where user_id = ".$attendee_id." and block_day>='".substr(to_mysql_datetime($startDate),0,10)."' and block_day<='".substr(to_mysql_datetime($endDate),0,10)."'";
+
+        $results = $this->db->query($qry)->result_array();
+
+        //Return true if $results is not empty, because there is at least one block day between both dates
+        return (!empty($results));
+    }
 
 }
