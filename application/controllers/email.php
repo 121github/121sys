6bcc66c37d1f1a515d1eb95f6a64054d706b4288
Brad Foster <bradf@121customerinsight.co.uk>
2015-04-27 16:45:37 +0100

    added email history

--- application/controllers/email.php
+++ application/controllers/email.php
@@ -420,10 +420,9 @@ class Email extends CI_Controller
 								'pending' => 0
 							);
 							$email_id = $this->Email_model->add_new_email_history($email_history);
-							$this->Email_model->set_email_outcome($email['urn']);
-							$this->Email_model->set_record_history($email_history);
 						}
-						
+						$this->Email_model->set_record_history($email);
+						$this->Email_model->set_email_outcome($email['urn']);
 						//Add the attachments to the email_history_attachments table
 						foreach($attachments as $attachment) {
 							$this->Email_model->insert_attachment_by_email_id($email_id, $attachment);
