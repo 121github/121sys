91170a527bbe34ce9cdf21c28be5a07916c687a6
Brad Foster <bradf@121customerinsight.co.uk>
2015-05-19 10:57:32 +0100

    fixed permission bug

--- application/views/templates/default.php
+++ application/views/templates/default.php
@@ -446,7 +446,9 @@ if (isset($_SESSION['current_campaign']) && in_array("show footer", $_SESSION['p
 <script src="<?php echo base_url(); ?>assets/js/main.js"></script>
 <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?v=3.exp"></script>
 <script type="text/javascript"> helper.baseUrl = '<?php echo base_url(); ?>' + '';
+<?php if(isset($_SESSION['permissions'])){ ?>
 helper.permissions = $.parseJSON('<?php echo json_encode(array_flip($_SESSION['permissions'])) ?>');
+<?php } ?>
 modals.init();
     <?php if(isset($_SESSION['user_id'])){ ?>
     check_session();
