29b3c34a5d4eab5cf032b88f9d2761cf0584b855
estebanc <estebanc@121customerinsight.co.uk>
2015-05-20 15:04:40 +0100

    Fixing problem with the nbf_dash.php file (syntax error, unexpected end of file in application/views/dashboard/nbf_dash.php on line 128).

--- application/views/dashboard/nbf_dash.php
+++ application/views/dashboard/nbf_dash.php
@@ -1,127 +1,141 @@
-
-      <div class="row">
-        <div class="col-lg-12">
-          <h1 class="page-header">New Business Dashboard</h1>
-        </div>
-        <!-- /.col-lg-12 --> 
-      </div>
-      <!-- /.row -->
-      <div class="row">
-        <div class="col-lg-8">
-                  <div class="panel panel-primary">
-            <div class="panel-heading"> <i class="fa fa-bar-chart-o fa-fw"></i>My Favorites     <?php if(!isset($_SESSION['current_campaign'])){ ?> <div class="pull-right">
-                <div class="btn-group">
-                  <button type="button" class="btn btn-default btn-xs dropdown-toggle" data-toggle="dropdown"> Campaign <span class="caret"></span> </button>
-                  <ul class="dropdown-menu pull-right" role="menu">
-                    <?php foreach($campaigns as $row): ?>
-                    <li><a href="#" class="interest-filter" id="<?php echo $row['id'] ?>"><?php echo $row['name'] ?></a> </li>
-                    <?php endforeach ?>
-                    <li class="divider"></li>
-                    <li><a class="interest-filter" ref="#">Show All</a> </li>
-                  </ul>
-                </div>
-              </div><? } ?></div>
-              <div class="panel-body favorites-panel">
-             <img src="<?php echo base_url(); ?>assets/img/ajax-loader-bar.gif" />
+<div class="row">
+    <div class="col-lg-12">
+        <h1 class="page-header">New Business Dashboard</h1>
+    </div>
+    <!-- /.col-lg-12 -->
+</div>
+<!-- /.row -->
+<div class="row">
+    <div class="col-lg-8">
+        <div class="panel panel-primary">
+            <div class="panel-heading"><i class="fa fa-bar-chart-o fa-fw"></i>My
+                Favorites     <?php if (!isset($_SESSION['current_campaign'])) { ?>
+                    <div class="pull-right">
+                    <div class="btn-group">
+                        <button type="button" class="btn btn-default btn-xs dropdown-toggle" data-toggle="dropdown">
+                            Campaign <span class="caret"></span></button>
+                        <ul class="dropdown-menu pull-right" role="menu">
+                            <?php foreach ($campaigns as $row): ?>
+                                <li><a href="#" class="interest-filter"
+                                       id="<?php echo $row['id'] ?>"><?php echo $row['name'] ?></a></li>
+                            <?php endforeach ?>
+                            <li class="divider"></li>
+                            <li><a class="interest-filter" ref="#">Show All</a></li>
+                        </ul>
+                    </div>
+                    </div><? } ?></div>
+            <div class="panel-body favorites-panel">
+                <img src="<?php echo base_url(); ?>assets/img/ajax-loader-bar.gif"/>
             </div>
         </div>
-        
-           <div class="panel panel-primary">
-            <div class="panel-heading"> <i class="fa fa-bar-chart-o fa-fw"></i>Interest Nows
-            <?php if(!isset($_SESSION['current_campaign'])){ ?> <div class="pull-right">
-                <div class="btn-group">
-                  <button type="button" class="btn btn-default btn-xs dropdown-toggle" data-toggle="dropdown"> Campaign <span class="caret"></span> </button>
-                  <ul class="dropdown-menu pull-right" role="menu">
-                    <?php foreach($campaigns as $row): ?>
-                    <li><a href="#" class="interest-filter" id="<?php echo $row['id'] ?>"><?php echo $row['name'] ?></a> </li>
-                    <?php endforeach ?>
-                    <li class="divider"></li>
-                    <li><a class="interest-filter" ref="#">Show All</a> </li>
-                  </ul>
-                </div>
-              </div><? } ?>
+
+        <div class="panel panel-primary">
+            <div class="panel-heading"><i class="fa fa-bar-chart-o fa-fw"></i>Interest Nows
+                <?php if (!isset($_SESSION['current_campaign'])) { ?>
+                    <div class="pull-right">
+                    <div class="btn-group">
+                        <button type="button" class="btn btn-default btn-xs dropdown-toggle" data-toggle="dropdown">
+                            Campaign <span class="caret"></span></button>
+                        <ul class="dropdown-menu pull-right" role="menu">
+                            <?php foreach ($campaigns as $row): ?>
+                                <li><a href="#" class="interest-filter"
+                                       id="<?php echo $row['id'] ?>"><?php echo $row['name'] ?></a></li>
+                            <?php endforeach ?>
+                            <li class="divider"></li>
+                            <li><a class="interest-filter" ref="#">Show All</a></li>
+                        </ul>
+                    </div>
+                    </div><? } ?>
             </div>
             <!-- /.panel-heading -->
             <div class="panel-body interest-panel">
-              <img src="<?php echo base_url(); ?>assets/img/ajax-loader-bar.gif" />
+                <img src="<?php echo base_url(); ?>assets/img/ajax-loader-bar.gif"/>
             </div>
-            <!-- /.panel-body --> 
-          </div>
-          
-      
-           <!-- /col-lg-8 -->
+            <!-- /.panel-body -->
+        </div>
 
-</div>
-<div class="col-lg-4">
-<?php if(@$_SESSION['current_campaign']==3){ ?>
-                  <div class="panel panel-primary">
-            <div class="panel-heading"> <i class="fa fa-bar-chart-o fa-fw"></i>Pending
-              </div>
-              <div class="panel-body pending-panel">
-             <img src="<?php echo base_url(); ?>assets/img/ajax-loader-bar.gif" />
+
+        <!-- /col-lg-8 -->
+
+    </div>
+    <div class="col-lg-4">
+        <?php if (@$_SESSION['current_campaign'] == 3) { ?>
+            <div class="panel panel-primary">
+                <div class="panel-heading"><i class="fa fa-bar-chart-o fa-fw"></i>Pending
+                </div>
+                <div class="panel-body pending-panel">
+                    <img src="<?php echo base_url(); ?>assets/img/ajax-loader-bar.gif"/>
+                </div>
+            </div>
+        <?php } ?>
+        <div class="panel panel-primary">
+            <div class="panel-heading"><i
+                    class="fa fa-bar-chart-o fa-fw"></i>Urgent <?php if (!isset($_SESSION['current_campaign'])) { ?>
+                    <div class="pull-right">
+                    <div class="btn-group">
+                        <button type="button" class="btn btn-default btn-xs dropdown-toggle" data-toggle="dropdown">
+                            Campaign <span class="caret"></span></button>
+                        <ul class="dropdown-menu pull-right" role="menu">
+                            <?php foreach ($campaigns as $row): ?>
+                                <li><a href="#" class="interest-filter"
+                                       id="<?php echo $row['id'] ?>"><?php echo $row['name'] ?></a></li>
+                            <?php endforeach ?>
+                            <li class="divider"></li>
+                            <li><a class="interest-filter" ref="#">Show All</a></li>
+                        </ul>
+                    </div>
+                    </div><? } ?>
+            </div>
+
+
+            <div class="panel-body urgent-panel">
+                <img src="<?php echo base_url(); ?>assets/img/ajax-loader-bar.gif"/>
             </div>
         </div>
-           <?php } ?>
-                <div class="panel panel-primary">
-            <div class="panel-heading"> <i class="fa fa-bar-chart-o fa-fw"></i>Urgent <?php if(!isset($_SESSION['current_campaign'])){ ?> <div class="pull-right">
-                <div class="btn-group">
-                  <button type="button" class="btn btn-default btn-xs dropdown-toggle" data-toggle="dropdown"> Campaign <span class="caret"></span> </button>
-                  <ul class="dropdown-menu pull-right" role="menu">
-                    <?php foreach($campaigns as $row): ?>
-                    <li><a href="#" class="interest-filter" id="<?php echo $row['id'] ?>"><?php echo $row['name'] ?></a> </li>
-                    <?php endforeach ?>
-                    <li class="divider"></li>
-                    <li><a class="interest-filter" ref="#">Show All</a> </li>
-                  </ul>
-                </div>
-              </div><? } ?>
-              </div>
-           
-              
-              <div class="panel-body urgent-panel">
-<img src="<?php echo base_url(); ?>assets/img/ajax-loader-bar.gif" />
+        <div class="panel panel-primary">
+            <div class="panel-heading"><i
+                    class="fa fa-bar-chart-o fa-fw"></i>Appointments <?php if (!isset($_SESSION['current_campaign'])) { ?>
+                    <div class="pull-right">
+                    <div class="btn-group">
+                        <button type="button" class="btn btn-default btn-xs dropdown-toggle" data-toggle="dropdown">
+                            Campaign <span class="caret"></span></button>
+                        <ul class="dropdown-menu pull-right" role="menu">
+                            <?php foreach ($campaigns as $row): ?>
+                                <li><a href="#" class="interest-filter"
+                                       id="<?php echo $row['id'] ?>"><?php echo $row['name'] ?></a></li>
+                            <?php endforeach ?>
+                            <li class="divider"></li>
+                            <li><a class="interest-filter" ref="#">Show All</a></li>
+                        </ul>
+                    </div>
+                    </div><? } ?>
             </div>
+            <div class="panel-body appointments-panel">
+                <img src="<?php echo base_url(); ?>assets/img/ajax-loader-bar.gif"/>
             </div>
-                            <div class="panel panel-primary">
-            <div class="panel-heading"> <i class="fa fa-bar-chart-o fa-fw"></i>Appointments <?php if(!isset($_SESSION['current_campaign'])){ ?> <div class="pull-right">
-                <div class="btn-group">
-                  <button type="button" class="btn btn-default btn-xs dropdown-toggle" data-toggle="dropdown"> Campaign <span class="caret"></span> </button>
-                  <ul class="dropdown-menu pull-right" role="menu">
-                    <?php foreach($campaigns as $row): ?>
-                    <li><a href="#" class="interest-filter" id="<?php echo $row['id'] ?>"><?php echo $row['name'] ?></a> </li>
-                    <?php endforeach ?>
-                    <li class="divider"></li>
-                    <li><a class="interest-filter" ref="#">Show All</a> </li>
-                  </ul>
-                </div>
-              </div><? } ?>
-              </div>
-              <div class="panel-body appointments-panel">
-<img src="<?php echo base_url(); ?>assets/img/ajax-loader-bar.gif" />
-            </div>
-            </div>
-        <!-- /.col-lg-4 --> 
-   </div>
-      <!-- /.row --> 
+        </div>
+        <!-- /.col-lg-4 -->
+    </div>
+    <!-- /.row -->
+</div>
+    <script src="<?php echo base_url() ?>assets/js/plugins/metisMenu/jquery.metisMenu.js"></script>
 
-<script src="<?php echo base_url() ?>assets/js/plugins/metisMenu/jquery.metisMenu.js"></script> 
+    <!-- Page-Level Plugin Scripts - Dashboard -->
+    <script src="<?php echo base_url() ?>assets/js/plugins/morris/raphael-2.1.0.min.js"></script>
+    <script src="<?php echo base_url() ?>assets/js/plugins/morris/morris.js"></script>
 
-<!-- Page-Level Plugin Scripts - Dashboard --> 
-<script src="<?php echo base_url() ?>assets/js/plugins/morris/raphael-2.1.0.min.js"></script> 
-<script src="<?php echo base_url() ?>assets/js/plugins/morris/morris.js"></script> 
-
-<!-- SB Admin Scripts - Include with every page --> 
-<script src="<?php echo base_url() ?>assets/js/sb-admin.js"></script> 
-<script>
-	$(document).ready(function(){
-		dashboard.urgent_panel();
-		dashboard.favorites_panel(3);
-		dashboard.appointments_panel();
-		dashboard.pending_panel();
-		dashboard.interest_panel(<?php echo ($_SESSION['role']>1?$_SESSION['user_id']:"false") ?>);
-				$(document).on("click",".interest-filter",function(e){
-			e.preventDefault();
-			dashboard.interest_panel(<?php echo ($_SESSION['role']>1?$_SESSION['user_id']:"false") ?>,$(this).attr('id'))
-		});
-	});
-	</script> 
+    <!-- SB Admin Scripts - Include with every page -->
+    <script src="<?php echo base_url() ?>assets/js/sb-admin.js"></script>
+    <script>
+        $(document).ready(function () {
+            dashboard.urgent_panel();
+            dashboard.favorites_panel(3);
+            dashboard.appointments_panel();
+            dashboard.pending_panel();
+            dashboard.interest_panel(<?php echo ($_SESSION['role']>1?$_SESSION['user_id']:"false") ?>);
+            $(document).on("click", ".interest-filter", function (e) {
+                e.preventDefault();
+                dashboard.interest_panel(<?php echo ($_SESSION['role']>1?$_SESSION['user_id']:"false") ?>, $(this).attr('id'))
+            });
+        });
+    </script>
