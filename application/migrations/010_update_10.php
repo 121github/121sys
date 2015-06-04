d3cca0e21db39d1744445acf1d0a32b6bce9c892
Brad Foster <bradf@121customerinsight.co.uk>
2015-06-04 09:17:21 +0100

    changes to mig 10

--- application/migrations/010_update_10.php
+++ application/migrations/010_update_10.php
@@ -35,8 +35,8 @@ class Migration_update_10 extends CI_Migration
 ");
 
 			
-		$this->db->query("insert ignore into record_keywords (select urn,subsector_name from subsectors left join company_subsectors using(subsector_id) where subsector_id < 250)");
-		$this->db->query("insert ignore into record_keywords (select urn,sector_name from subsectors left join company_subsectors using(subsector_id) left join sectors using(sector_id) where sector_id < 13)");
+		$this->db->query("insert ignore into record_keywords (select urn,subsector_name from subsectors left join company_subsectors using(subsector_id) left join companies using(company_id) where subsector_id < 250)");
+		$this->db->query("insert ignore into record_keywords (select urn,sector_name from subsectors left join company_subsectors using(subsector_id) left join companies using(company_id) left join sectors using(sector_id) where sector_id < 13)");
 		
         $this->db->query("update company_subsectors set subsector_id = 66220 where subsector_id = 239");
         $this->db->query("update company_subsectors set subsector_id = 66220 where subsector_id = 235");
