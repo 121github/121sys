2e6abd0549c1e061c9b9aba6c4256f77326fd4ed
Brad Foster <bradf@121customerinsight.co.uk>
2015-06-29 09:00:34 +0100

    updated mig 19

--- application/migrations/019_update_19.php
+++ application/migrations/019_update_19.php
@@ -14,8 +14,8 @@ class Migration_update_19 extends CI_Migration
 
 $this->db->query("ALTER TABLE `record_details_fields` ADD `editable` TINYINT NOT NULL DEFAULT '1'");
 $this->db->query("ALTER TABLE `webform_answers` CHANGE `updated_on` `updated_on` TIMESTAMP on update CURRENT_TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP");
-$this->db->query("ALTER TABLE `webform_answers` ADD `completed_on` DATETIME NULL DEFAULT NULL AFTER `updated_by`");
 $this->db->query("ALTER TABLE `webform_answers` ADD `completed_by` INT NULL DEFAULT NULL AFTER `competed_on`") ;
+$this->db->query("ALTER TABLE `webform_answers` ADD `completed_on` DATETIME NULL DEFAULT NULL AFTER `updated_by`");
 $this->db->query("update webform_answers set updated_on = null where updated_on = '0000-00-00 00:00:00'");
 $this->db->query("ALTER TABLE `webform_answers` ADD `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY FIRST");
 	}
