5d85393dafa4449fc745a56eaa494d984a7e400a
Brad Foster <bradf@121customerinsight.co.uk>
2015-06-30 13:25:15 +0100

    updated tv cont

--- assets/ghs_tool.php
+++ assets/ghs_tool.php
@@ -45,7 +45,7 @@
 <body>
 <div class="row">
 <div class="col-lg-12">
-<form id="form">
+<form id="form" style="pading:20px">
 <div class="form-group">
 <select class="selectpicker" id="function">
 <option value="trackvia/review_required">Send for prequal</option>
@@ -53,12 +53,17 @@
 <option value="trackvia/add_appointment">Add appointment</option>
 <option value="trackvia/already_had_survey">Already had survey</option>
 <option value="trackvia/unable_to_contact">Unable to contact</option>
-
+<option value="trackvia/review_required">Send for desktop prequal</option>
+<option value="trackvia/notified_not_eligible">Notified not eligible</option>
 </select>
 </div>
 <div class="form-group">
 <label>URN</label>
 <input class="form-control" id="urnval" name="urn"/>
+</div>
+<div class="form-group">
+<label>Trackvia Record locator</label>
+<input class="form-control" id="tv" disabled placeholder="Not ready yet" name="tv"/>
 </div>
 <div class="form-group">
 <button class="btn btn-primary" id="update">Update</button>
@@ -79,7 +84,7 @@ $('#update').click(function(e){
 		url:"https://www.121system.com/"+$('#function').val(),
 	type:"POST",
 	dataType:"HTML",
-	data:{ urn:$('#urnval').val() }
+	data:{ urn:$('#urnval').val() ,tv:$('#tv').val() }
 	}).done(function(response){
 		$('#response').html(response);
 	});
