9a647c4d1fd475072594ec74c738e84707c70add
Brad Foster <bradf@121customerinsight.co.uk>
2015-07-06 11:20:27 +0100

    added mig 21

---
+++ application/migrations/021_update_21.php
@@ -0,0 +1,21 @@
+
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_update_21 extends CI_Migration
+{
+    public function __construct()
+    {
+        $this->load->model('Database_model');
+    }
+
+    public function up()
+    {
+        $this->firephp->log("starting migration 21");
+$this->db->query("ALTER TABLE record_details_fields DROP COLUMN is_radio");
+        $this->db->query("ALTER TABLE `record_details_fields` ADD `is_radio` INT NULL DEFAULT NULL AFTER `is_select`");
+		
+		$this->db->query("ALTER TABLE `record_details_fields` CHANGE `field_name` `field_name` VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL");
+    }
+
+}
