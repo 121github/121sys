80175f20a9b0dc399ce32fad47808ecfb4410a54
Brad Foster <bradf@121customerinsight.co.uk>
2015-07-06 14:52:59 +0100

    added direction check

--- assets/js/detail2.js
+++ assets/js/detail2.js
@@ -15,7 +15,13 @@ var record = {
     init: function (urn, role, campaign) {
 		 $(document).on('click', '#update-record', function (e) {
                 e.preventDefault();
-                if ($('.outcomepicker').val().length > 0) {
+			 if ($('#custom-panel:contains("Call Direction")')) {
+				if($('#custom-panel:contains("Inbound")').length==0&&$('#custom-panel:contains("Outbound")').length==0){
+				alert("You must set a call direction");
+			  }
+			 }
+				
+			if ($('.outcomepicker').val().length > 0) {
                     if ($('.outcomepicker').val() == "4" && $('.history-panel').find('tbody tr').length > 0) {
                         modal.dead_line($(this));
                     } else if($('.outcomepicker').val() == "120"&& $('.contact-panel').find('#map-link').length > 0){
