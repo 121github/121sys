5fec74b8bf8936121c81d4c29206f727f6e082eb
Brad Foster <bradf@121customerinsight.co.uk>
2015-07-08 08:58:56 +0100

    fixed mug 23

--- application/migrations/023_update_23.php
+++ application/migrations/023_update_23.php
@@ -13,6 +13,19 @@ class Migration_update_23 extends CI_Migration
     {
         $this->firephp->log("starting migration 23");
 
+        $this->db->query("CREATE TABLE IF NOT EXISTS `record_tasks` (
+  `id` int(11) NOT NULL,
+  `urn` int(11) NOT NULL,
+  `task_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `task_status_id` int(11) NOT NULL,
+  `added_on` datetime NOT NULL,
+  `updated_by` int(11) NOT NULL,
+  `updated_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  KEY `urn` (`urn`,`task_id`,`user_id`,`task_status_id`),
+  KEY `task_status_id` (`task_status_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1");
+
         $this->db->query("
 CREATE TABLE IF NOT EXISTS `campaign_tasks` (
   `campaign_id` int(11) NOT NULL,
@@ -45,8 +58,6 @@ MODIFY `task_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1");
 
      $this->db->query("ALTER TABLE `task_status`
 MODIFY `task_status_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1");
-
-     $this->db->query("ALTER TABLE `record_tasks` CHANGE `added_on` `added_on` DATETIME NULL DEFAULT CURRENT_TIMESTAMP");
 	 
 	  $this->db->query("alter table `record_tasks` add unique(urn,task_id)");
 
