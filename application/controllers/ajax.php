2d58403c8a704f3517a7f34c84399f3ebc83e865
estebanc <estebanc@121customerinsight.co.uk>
2015-07-17 15:15:06 +0100

    Fixing problem with the ownership panel. It does not show the owners successfully

--- application/controllers/ajax.php
+++ application/controllers/ajax.php
@@ -802,7 +802,11 @@ $d = preg_replace('/[0-9]/','',$data['description']);
     public function get_ownership()
     {
         if ($this->input->is_ajax_request()) {
-            $owners = $result = $this->Records_model->get_ownership(intval($this->input->post("urn")));
+            $result = $this->Records_model->get_ownership(intval($this->input->post("urn")));
+            $owners = array();
+            foreach ($result as $row) {
+                $owners[] = $row['user_id'];
+            }
             echo json_encode(array(
                 "success" => true,
                 "data" => $owners
