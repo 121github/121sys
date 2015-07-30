0900b79ae9670424f1f87b3592a593a971a63b15
estebanc <estebanc@121customerinsight.co.uk>
2015-07-30 15:14:58 +0100

    [Log Error][Search] Undefined indexes #121SYS-97 fixed

--- application/controllers/search.php
+++ application/controllers/search.php
@@ -296,7 +296,7 @@ class Search extends CI_Controller
         
         //this turns the url into an key=>val array starting from the given segment (3) because thats after the search(1)/custom(2) part
         $uri   = $this->uri->uri_to_assoc(4);
-		if(@intval($uri['campaign']>0)){
+		if(isset($uri['campaign']) && @intval($uri['campaign']>0)){
 			$_SESSION['current_campaign']=$uri['campaign'];
 		}
         //this gets the table we want to look at - typically the records table or the history table
