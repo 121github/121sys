ad79eca1b4fcaf7eb7017bbd9880072c32e5e669
Brad Foster <bradf@121customerinsight.co.uk>
2015-07-31 15:09:30 +0100

    added take ownership permission

---
+++ application/migrations/034_update_34.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_update_34 extends CI_Migration
+{
+    public function __construct()
+    {
+        $this->load->model('Database_model');
+    }
+
+    public function up()
+    {
+        $this->firephp->log("starting migration 34");
+		$this->db->query("INSERT INTO `permissions` (`permission_id`, `permission_name`, `permission_group`) VALUES (NULL, 'take ownership', 'System')");
+	}
+	
+}
+
+?>
