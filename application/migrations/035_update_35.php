c94b35d77333893087d22866abb97f58e0690f6b
Brad Foster <bradf@121customerinsight.co.uk>
2015-08-05 15:32:58 +0100

    max dials changed

--- application/migrations/035_update_35.php
+++ application/migrations/035_update_35.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Migration_update_34 extends CI_Migration
+class Migration_update_35 extends CI_Migration
 {
     public function __construct()
     {
