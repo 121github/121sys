bd40ac99728880de590e96bdbbca6a34214d4509
Brad Foster <bradf@121customerinsight.co.uk>
2015-08-14 13:28:18 +0100

    fixed slots

--- application/controllers/trackvia.php
+++ application/controllers/trackvia.php
@@ -120,6 +120,14 @@ class Trackvia extends CI_Controller
 
     }
 
+	public function fix_non_contacts(){
+			$query = "select urn from records where outcome_id = 137 and campaign_id in (22,29)";
+		foreach($this->db->query($query)->result_array() as $row){
+		$urn 	= $row['urn'];
+		$this->unable_to_contact($urn);
+		}
+	}
+
     public function get_rebookings()
     {
         if (isset($_POST['campaign']) && @$_POST['campaign']) {
@@ -284,7 +292,16 @@ class Trackvia extends CI_Controller
         $this->db->query("update records set map_icon ='fa-home' where campaign_id in(22,28,29)");
         $this->db->query("update contact_addresses left join contacts using(contact_id) left join records using(urn) set contact_addresses.`primary` = 1 where campaign_id in(22,28,29)");
         $this->db->query("update contacts inner join records using(urn)     inner join data_sources using(source_id) set notes = source_name where campaign_id in(22,28,29) and records.source_id is not null");
-        $this->db->query("update records left join campaigns using(campaign_id) set outcome_id = 137,outcome_reason_id=NULL, record_status=3 where outcome_id in(select outcome_id from outcomes where delay_hours is not null) and dials > max_dials and campaign_id in(22,28,29)");
+		
+		$query = "select urn from records where outcome_id in(select outcome_id from outcomes where delay_hours is not null) and dials > max_dials and campaign_id in(22,28,29)";
+		foreach($this->db->query($query)->result_array() as $row){
+		$urn 	= $row['urn'];
+		$this->db->query("update records left join campaigns using(campaign_id) set outcome_id = 137,outcome_reason_id=NULL, record_status=3 where urn = '$urn'");
+		$this->unable_to_contact($urn);
+		}
+		
+		
+		
         $this->db->query("update records set urgent = null where source_id not in(38,35) and campaign_id in(22,28,29)");
         //Update appointmentes without history associated
         echo "<br>Checking if exists appointmentes without history associated...";
@@ -762,9 +779,10 @@ class Trackvia extends CI_Controller
 
     }
 
-    public function unable_to_contact()
-    {
+    public function unable_to_contact($urn=false)
+    {	if(!$urn){
         $urn = $this->input->post('urn');
+	}
         //Get the record data
         $record = $this->get_record($urn);
 		
