b1eb856ea293bf2bf9a0bdf70c8a97a647c7c875
Brad Foster <bradf@121customerinsight.co.uk>
2015-08-20 17:28:43 +0100

    fixed email

--- application/controllers/email.php
+++ application/controllers/email.php
@@ -523,7 +523,8 @@ class Email extends CI_Controller
 		
     	$this->load->library('email');
     	
-    	$config = array("smtp_host"=>"mail.121system.com",
+    	$config = array("protocol"=>"smtp",
+		"smtp_host"=>"mail.121system.com",
 		"smtp_user"=>"mail@121system.com",
 		"smtp_pass"=>"L3O9QDirgUKXNE7rbNkP",
 		"smtp_port"=>25);
