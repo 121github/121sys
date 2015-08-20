4a50813826b284f46fce4f5f052a0e906a3b7485
Brad Foster <bradf@121customerinsight.co.uk>
2015-08-20 08:50:15 +0100

    hsl

--- application/migrations/044_update_44.php
+++ application/migrations/044_update_44.php
@@ -12,7 +12,7 @@ class Migration_update_44 extends CI_Migration
     public function up()
     {
 				
-		$this->db->query("INSERT INTO `permissions` (`permission_id`, `permission_name`, `permission_group`) VALUES (NULL, 'apps to planner', 'Appointments'");
+		$this->db->query("INSERT INTO `permissions` (`permission_id`, `permission_name`, `permission_group`) VALUES (NULL, 'apps to planner', 'Appointments')");
 		$id = $this->db->insert_id();
 		$this->db->query("INSERT INTO `role_permissions` (`role_id`, `permission_id`) VALUES ('7', $id)");
 	}
