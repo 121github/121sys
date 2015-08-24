2e50a13af72f72650e1b76549597f5d466795cc0
Brad Foster <bradf@121customerinsight.co.uk>
2015-08-24 11:14:27 +0100

    fixed minDate

--- assets/js/modals.js
+++ assets/js/modals.js
@@ -647,7 +647,7 @@ var modals = {
         //this function automatically sets the end date for the appointment 1 hour ahead of the start date
         $(".startpicker").on("dp.hide", function (e) {
             var m = moment(e.date, "DD\MM\YYYY HH:mm");
-            $('.endpicker').data("DateTimePicker").minDate(e.date);
+            $('.endpicker').data("DateTimePicker").date(e.date);
             $('.endpicker').data("DateTimePicker").date(m.add('hours', 1).format('DD\MM\YYYY HH:mm'));
         });
         $("#modal").find("#tabs").tab();
