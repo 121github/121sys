e20dfcf605d17c9c08c9cf64011560f6c2fbe455
Brad Foster <bradf@121customerinsight.co.uk>
2015-10-12 10:11:56 +0100

    added clk insurance webform

--- application/views/records/panels/webform.php
+++ application/views/records/panels/webform.php
@@ -4,7 +4,7 @@
         <div class="panel-content"> 
           <?php $x=0; foreach($webforms as $webform){ $x++; ?>
           <?php if($x>1){ echo "<br />"; } ?><a href="<?php echo base_url()."webforms/edit/".$webform['campaign_id']."/".$details['record']['urn']."/".$webform['webform_id']; ?>"><?php echo $webform['webform_name'] ?></a> <?php if(!empty($webform['completed_on'])){ ?><small class="text-success"><span class="fa fa-check-circle"></span>  Completed: <?php echo $webform['completed_on']; ?> by <?php echo $webform['name']; ?> </small><?php } else { ?><small class="text-danger"><span class="fa fa-exclamation-circle"></span> Incomplete</small><?php } ?>
-          <?php } ?>
+          <?php } ?> 
         </div>
       </div>
     </div>
