3c7e47050f5aa4c0f972474c8be975f0b929a2b6
Brad Foster <bradf@121customerinsight.co.uk>
2015-10-16 14:40:40 +0100

    added 65 mig

--- application/migrations/065_update_65.php
+++ application/migrations/065_update_65.php
@@ -7,6 +7,8 @@ class Migration_update_65 extends CI_Migration
     public function __construct()
     {
         $this->load->model('Database_model');
+				$this->db2 = $this->load->database('uk_postcodes',true);
+
     }
 
     public function up()
@@ -14,8 +16,6 @@ class Migration_update_65 extends CI_Migration
         $this->firephp->log("starting migration 65");
 $this->db->query("ALTER TABLE `locations` CHANGE `lat` `lat` DECIMAL(18,12) NULL DEFAULT NULL");
 $this->db->query("ALTER TABLE `locations` CHANGE `lng` `lng` DECIMAL(18,12) NULL DEFAULT NULL");
-
-$this->db->query("insert ignore into uk_postcodes.PostcodeIo (postcode,latitude,longitude) select postcode,lat,lng from uk_postcodes");
 
 $this->db->query("delete from locations");
 $this->db->query("update contact_addresses set location_id = null");
