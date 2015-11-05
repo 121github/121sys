38dd8f904423b4e8981af6658f2f1eb0e7f71a8e
Brad Foster <bradf@121customerinsight.co.uk>
2015-11-05 12:49:34 +0000

    added tricheckbox

---
+++ application/views/admin/campaign_permissions.php
@@ -0,0 +1,47 @@
+
+    <div class="row">
+      <div class="col-lg-12">
+        <h1 class="page-header">Campaign Permissions <small>Override role permissions for a campaign</small></h1>
+      </div>
+      <!-- /.col-lg-12 --> 
+    </div>
+    <!-- /.row -->
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="panel panel-primary" id="campaign-permissions-panel">
+          <div class="panel-heading">Campaign Permissions
+          </div>
+          <!-- /.panel-heading -->
+          <div class="panel-body campaign-permissions-data">
+            <?php $this->view('forms/edit_campaign_permissions_form.php'); ?>
+            <table class="table ajax-table">
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Name</th>
+                  <th>Options</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td colspan="3"><img src="<?php echo base_url(); ?>assets/img/ajax-loader-bar.gif" /></td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <!-- /.panel-body --> 
+        </div>
+      </div>
+      
+      <!-- /.row --> 
+
+<script src="<?php echo base_url() ?>assets/js/plugins/metisMenu/jquery.metisMenu.js"></script> 
+
+<!-- SB Admin Scripts - Include with every page --> 
+<script src="<?php echo base_url() ?>assets/js/sb-admin.js"></script> 
+<script>
+$(document).ready(function(){
+	admin.init();
+	admin.campaign_permissions.init();
+});
+</script> 
---
+++ application/views/forms/edit_campaign_permissions_form.php
@@ -0,0 +1,20 @@
+      <form style="display:none; padding:10px 20px;" id="cp-form">
+      <input type="hidden" name="campaign_id">
+           <ul class="list-group">
+       <?php foreach($permissions as $group_name => $group_permissions){ ?>
+    <li class="list-group-item" style="height:auto"><?php echo $group_name ?><span class="pull-right" style="width:80%">
+    <?php foreach($group_permissions as $id=>$name){ ?>
+    <div class="checkbox-group" style="display:inline-block; width:170px">
+    <input id="pm_<?php echo $id ?>" type="hidden" name="permission[<?php echo $id ?>]" />
+    <input id="cb_<?php echo $id ?>" type="checkbox" ><label class="cbx-label"><?php echo $name ?></label></div ><?php } ?>
+</span>
+<div class="clearfix"></div>
+    </li>
+    <?php } ?>
+  </ul>
+    
+       <div class="form-actions pull-right">
+         <button class="marl btn btn-default close-btn">Cancel</button>
+         <button type="submit" class="marl btn btn-primary save-btn">Save</button>
+        </div>
+      </form>
