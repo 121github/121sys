9c075de823e49e4155bff509079b9abd091b21ea
Brad Foster <bradf@121customerinsight.co.uk>
2015-12-10 09:44:20 +0000

    fixed mig 78

--- application/migrations/078_update_78.php
+++ application/migrations/078_update_78.php
@@ -15,12 +15,16 @@ class Migration_update_78 extends CI_Migration
 		$check = $this->db->query("SHOW COLUMNS FROM `user_groups` LIKE 'theme_images'");
 		if(!$check->num_rows()){
 		$this->db->query("ALTER TABLE `user_groups` CHANGE `theme_folder` `theme_images` VARCHAR( 50 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL");
+		}
+			$check3 = $this->db->query("SHOW COLUMNS FROM `users` LIKE 'theme_color'");
+			if(!$check3->num_rows()){
 		$this->db->query("ALTER TABLE `user_groups` ADD `theme_color` VARCHAR( 50 ) NOT NULL");
 		$this->db->query("update user_groups set theme_color = theme_images");
-		}
+			}
 		$check2 = $this->db->query("SHOW COLUMNS FROM `users` LIKE 'theme_color'");
 			if(!$check2->num_rows()){
 			$this->db->query("ALTER TABLE `users` ADD `theme_color` VARCHAR( 50 ) NOT NULL");
+			
 			}
 
 	}
