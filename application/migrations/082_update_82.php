5940cc10344df697ca934c5e8a552ba33d5e37e3
Brad Foster <bradf@121customerinsight.co.uk>
2015-12-16 14:18:50 +0000

    added 82

---
+++ application/migrations/082_update_82.php
@@ -0,0 +1,43 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_update_82 extends CI_Migration
+{
+
+    public function __construct()
+    {
+        $this->load->model('Database_model');
+    }
+
+    public function up()
+    {
+        $this->firephp->log("starting migration 82");
+		
+		$this->db->query("alter table `appointment_slot_override` add unique(appointment_slot_id,user_id,date)");
+		
+		$qry = "insert ignore into appointment_slot_override (appointment_slot_id,user_id,max_slots,`date`,notes) select appointment_slot_id,user_id,'0',block_day,if(reason_id=3,other_reason,reason) from appointment_rules left join appointment_rule_reasons using(reason_id) where appointment_slot_id is not null";
+		$this->db->query($qry);
+		
+		$qry = "select *,if(reason_id=3,other_reason,reason) reason from appointment_rules left join appointment_rule_reasons using(reason_id) where appointment_slot_id is null";
+		
+		foreach($this->db->query($qry)->result_array() as $row){
+			//get the slots in use
+			$slots = $this->db->query("select appointment_slot_id from appointment_slot_assignment where user_id = '{$row['user_id']}' group by appointment_slot_id")->result_array();
+			
+			
+			foreach($slots as $slot){
+		$qry = "insert ignore into appointment_slot_override (appointment_slot_id,user_id,max_slots,`date`,notes) values({$slot['appointment_slot_id']},{$row['user_id']},'0','{$row['block_day']}','{$row['reason']}') ";
+		$this->db->query($qry);
+		
+			}
+		}
+		
+		$this->db->query("drop table if exists appointment_rules");
+$this->db->query("drop table if exists appointment_rule_reasons");
+		
+		
+	}
+	 public function down()
+    {
+	}
+}
