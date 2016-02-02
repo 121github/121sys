a6a1d0ad5c8e1225f7cec7db114123d24102f638
Brad Foster <bradf@121customerinsight.co.uk>
2016-02-02 11:54:29 +0000

    added map files

---
+++ application/migrations/092_update_92.php
@@ -0,0 +1,58 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_update_91 extends CI_Migration
+{
+
+    public function __construct()
+    {
+        $this->load->model('Database_model');
+    }
+
+    public function up()
+    {
+		$this->firephp->log("starting migration 92");
+		 $check = $this->db->query("SHOW COLUMNS FROM `datatables_columns` LIKE 'c10'");
+        if(!$check->num_rows()){
+			$this->db->query("INSERT IGNORE INTO `datatables_columns` (
+`column_id` ,
+`column_title` ,
+`column_alias` ,
+`column_select` ,
+`column_order` ,
+`column_group` ,
+`column_table`
+)
+VALUES 
+(NULL , 'c7', '', 'c7', 'c7', 'Extra Fields', 'record_details'),
+(NULL , 'c8', '', 'c8', 'c8', 'Extra Fields', 'record_details'),
+(NULL , 'c9', '', 'c9', 'c9', 'Extra Fields', 'record_details'),
+(NULL , 'c10', '', 'c10', 'c10', 'Extra Fields', 'record_details'),
+(NULL , 'd4', '', 'd4', 'd4', 'Extra Fields', 'record_details'),
+(NULL , 'd5', '', 'd5', 'd5', 'Extra Fields', 'record_details'),
+(NULL , 'd6', '', 'd6', 'd6', 'Extra Fields', 'record_details'),
+(NULL , 'd7', '', 'd7', 'd7', 'Extra Fields', 'record_details'),
+(NULL , 'd8', '', 'd8', 'd8', 'Extra Fields', 'record_details'),
+(NULL , 'd9', '', 'd9', 'd9', 'Extra Fields', 'record_details'),
+(NULL , 'd10', '', 'd10', 'd10', 'Extra Fields', 'record_details'),
+(NULL , 'dt4', '', 'dt4', 'dt4', 'Extra Fields', 'record_details'),
+(NULL , 'dt5', '', 'dt5', 'dt5', 'Extra Fields', 'record_details'),
+(NULL , 'dt6', '', 'dt6', 'dt6', 'Extra Fields', 'record_details'),
+(NULL , 'dt7', '', 'dt7', 'dt7', 'Extra Fields', 'record_details'),
+(NULL , 'dt8', '', 'dt8', 'dt8', 'Extra Fields', 'record_details'),
+(NULL , 'dt9', '', 'dt9', 'dt9', 'Extra Fields', 'record_details'),
+(NULL , 'dt10', '', 'dt10', 'dt10', 'Extra Fields', 'record_details'),
+(NULL , 'n4', '', 'n4', 'n4', 'Extra Fields', 'record_details'),
+(NULL , 'n5', '', 'n5', 'n5', 'Extra Fields', 'record_details'),
+(NULL , 'n6', '', 'n6', 'n6', 'Extra Fields', 'record_details'),
+(NULL , 'n7', '', 'n7', 'n7', 'Extra Fields', 'record_details'),
+(NULL , 'n8', '', 'n8', 'n8', 'Extra Fields', 'record_details'),
+(NULL , 'n9', '', 'n9', 'n9', 'Extra Fields', 'record_details'),
+(NULL , 'n10', '', 'n10', 'n10', 'Extra Fields', 'record_details')");
+		}
+	}
+	 public function down(){
+		 
+	 }
+	 
+}
---
+++ assets/js/view-map.js
@@ -0,0 +1,196 @@
+function full_table_reload() {
+    view.table.destroy();
+    view.table.destroy();
+    view.reload_table();
+}
+
+var view = {
+    init: function() {
+        this.table;
+		this.planner_permission = false;
+
+        $(document).on("click", ".group-filter", function(e) {
+            e.preventDefault();
+            $icon = $(this).closest('ul').prev('button').find('span');
+            $(this).closest('ul').prev('button').text($(this).text()).prepend($icon);
+            $(this).closest('form').find('input[name="group"]').val($(this).attr('id'));
+            $(this).closest('ul').find('a').css("color", "black");
+            $(this).css("color", "green");
+            maps.colour_by = $('.filter-form').find('input[name="group"]').val();
+            view.table.columns.adjust().draw();
+        });
+    },
+    get_used_icons: function() {
+        $.ajax({
+            url: helper.baseUrl + 'records/get_used_icons',
+            type: "POST",
+            dataType: "JSON"
+        }).done(function(response) {
+            $('.record-icon').iconpicker();
+            $('.record-icon').iconpicker('setIconset', {
+                iconClass: 'fa',
+                iconClassFix: '',
+                icons: response.icons
+            }).change(function(e) {
+                var icon = (e.icon == 'empty' ? 'Icon' : e.icon);
+                view.table.column($(this).attr('data-index')).search(icon).draw();
+            });
+        });
+    },
+    reload_table: function() {
+        var headings = "";
+        column_count = new Array();
+        $.each(table_columns.headings, function(i, header) {
+            headings += "<th>" + header + "</th>";
+            column_count[i] = i;
+        });
+        var table = "<table width='100%' class='table small table-striped table-bordered table-hover data-table'><thead><tr>" + headings + "</tr></thead>";
+        table += "<tfoot><tr>" + headings + "</tr></tfoot></table>";
+
+        $('#view-container').html(table);
+        view.populate_table();
+    },
+    populate_table: function(table_name) {
+        var start_time = new Date().getTime();
+        view.table = $('.data-table').DataTable({
+            buttons: [
+                'copy', 'csv', 'excel', 'print'
+            ],
+            colReorder: true,
+            "oLanguage": {
+                "sProcessing": "<img src='" + helper.baseUrl + "assets/img/ajax-loader-bar.gif'>"
+            },
+            "dom": '<"row top-row"<"col-xs-12 col-sm-5"<"dt_info"i>r><"col-xs-12 col-sm-7"p>><"row"<"col-lg-12"t>><"row bottom-row"<"col-lg-12"<"pull-left"l> <"pull-right marl" B>>><"clear">',
+            "lengthMenu": [
+                [10, 25, 50, 100, 1000],
+                [10, 25, 50, 100, 1000]
+            ],
+            "width": "100%",
+            "scrollX": true,
+            "processing": true,
+            "serverSide": true,
+            "pagingType": "simple",
+            "iDisplayLength": 50,
+            responsive: true,
+            order: [
+                [0, "desc"]
+            ],
+            "ajax": {
+                url: helper.baseUrl + process_url,
+                type: 'POST',
+                beforeSend: function() {
+                    $('.dt_info div').empty()
+                    start_time = new Date().getTime();
+                    maps.items = [];
+					$('.loading-overlay').fadeIn();
+                },
+                data: function(d) {
+                    d.extra_field = false;
+                    d.bounds = maps.getBounds(),
+                    d.map = true;
+                    d.group = $('.filter-form').find('input[name="group"]').val();
+                },
+                error: function(xhr, error, thrown) {
+                    if (error == "parsererror") {
+                        alert("Oops! There was an error parsing the data.");
+                    }
+                },
+                complete: function(d) {
+					if(d.responseJSON.draw=="1"){
+					maps.render_display();	
+					view.table.columns.adjust()
+					}
+                    request_time = (new Date().getTime() - start_time) / 1000;
+                    $('.dt_info').show().find('div').append(' <span class="tt" data-html="true" data-toggle="tooltip" data-placement="bottom" title="Process time ' + Number(d.responseJSON.process_time) + ' seconds<br>Query time ' + Number(d.responseJSON.query_time) + ' seconds<br>Request time ' + request_time + ' seconds"><span class="glyphicon glyphicon-info-sign"></span></span>');
+                    $('.tt').tooltip();
+
+                    //Show the records in the map
+                    maps.showItems();
+                    maps.current_postcode = getCookie('current_postcode');
+                    view.planner_permission = d.responseJSON.planner_permission;
+                    maps.temp_bounds = false;
+					$('.loading-overlay').fadeOut();
+                    //Show search options if some filter exist
+                    if (view.has_filter) {
+                        $('.dataTables_info').append("<span class='glyphicon glyphicon-filter red modal-show-filter-options pointer'></span>");
+                    }
+                }
+            },
+            "deferRender": true,
+            "columns": table_columns.columns,
+            "columnDefs": [{
+                "targets": column_count,
+                "data": null,
+                "defaultContent": "-"
+            }],
+            "createdRow": function(row, data, dataIndex) {
+                $(row).attr('data-urn', data.urn);
+                $(row).attr('data-id', data.marker_id);
+				if(page_name=="records"){
+                $(row).attr('data-modal', 'view-record');
+				} else if(page_name=="appointment"){
+				$(row).attr('data-modal', 'view-appointment');	
+				}
+                $(row).attr('postcode', data['postcode']);
+                $(row).addClass('pointer');
+                if (data['change_type'] == "delete") {
+                    $(row).addClass('danger');
+                }
+                maps.items.push(data);
+            }
+        });
+
+        //filterable columns
+        $('.data-table tfoot th').each(function() {
+            var title = $('.data-table thead th').eq($(this).index()).text();
+            var filter_attribute = 'placeholder="Filter..."';
+            if (title == "Icon") {
+                var filter_attribute = "disabled";
+            }
+
+            if (title == "Options") {
+                vbp
+                $(this).html('');
+            } else if (title == "Icon") {
+                $icon_btn = $('<button class="btn btn-default btn-sm iconpicker record-icon" role="iconpicker" data-icon="" data-index="' + $(this).index() + '" data-iconset="fontawesome" style="color:#0066"></button>');
+                $(this).html($icon_btn);
+                view.get_used_icons();
+            } else {
+                var search_val = view.table.column($(this).index()).search();
+                if (typeof search_val[0] !== "undefined") {
+                    var filter_val = search_val[0];
+                } else {
+                    var filter_val = "";
+                }
+                $(this).html('<input class="dt-filter input-sm form-control" ' + filter_attribute + ' value="' + filter_val + '" />');
+            }
+        });
+
+        // Apply the search
+        view.table.columns().eq(0).each(function(colIdx) {
+            var run_filter = debounce(function() {
+				var order = view.table.colReorder.order();
+                view.table.column(order[colIdx]).search(this.value).draw();
+            }, 1000);
+            $('input', view.table.column(colIdx).footer()).on('keyup change', run_filter);
+        });
+
+
+        $("div.dataTables_scrollFootInner table tfoot tr").appendTo('div.dataTables_scrollHeadInner table thead');
+
+        $('#search_0').css('text-align', 'center');
+
+        view.table.on('column-reorder', function(e, settings, details) {
+            $.ajax({
+                url: helper.baseUrl + 'datatables/save_order',
+                type: "POST",
+                dataType: "JSON",
+                data: {
+                    columns: view.table.colReorder.order(),
+                    view: table_columns.view_id
+                }
+            })
+        });
+    },
+
+}
