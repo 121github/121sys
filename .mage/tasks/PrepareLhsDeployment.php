b068b114887f9c1040c0034a3ca94a1e7a321f00
estebanc <estebanc@121customerinsight.co.uk>
2016-02-11 14:35:09 +0000

    Adding htaccess for lhs #121SYS-291

--- .mage/tasks/PrepareLhsDeployment.php
+++ .mage/tasks/PrepareLhsDeployment.php
@@ -15,7 +15,7 @@ class PrepareLhsDeployment extends AbstractTask
     {
         $commandList = array(
             'mv application/config/database.php.lhsurveying application/config/database.php',
-            'mv .htacess_lhs .htacess',
+            'mv .htaccess_lhs .htaccess',
             'rm -rf application/config/database.php.*',
             'setfacl -R -m u:one2one:rwx -m u:\`whoami\`:rwx datafiles',
             'setfacl -dR -m u:one2one:rwx -m u:\`whoami\`:rwx datafiles',
