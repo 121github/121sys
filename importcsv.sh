77c8485d13c52b0c0c40541a9491fdffe0aec660
estebanc <estebanc@121customerinsight.co.uk>
2016-02-12 15:02:06 +0000

    #121SYS-293 fixed

--- importcsv.sh
+++ importcsv.sh
@@ -7,6 +7,9 @@ DELIM=","
 CSV="$1"
 TABLE="$2"
 
+USER="$4"
+PASS="$5"
+
 [ "$CSV" = "" -o "$TABLE" = "" ] && echo "Syntax: $0 csvfile tablename" && exit 1
 
 FIELDS=$(head -1 "$CSV" | sed -e 's/'$DELIM'/` varchar(255),\n`/g' -e 's/\r//g')
@@ -14,7 +17,7 @@ FIELDS='`'"$FIELDS"'` varchar(255)'
 
 #echo "$FIELDS" && exit
 
-mysql --user=bradf --password=brad123 $DB -e "
+mysql --user="$USER" --password="$PASS" $DB -e "
 DROP TABLE IF EXISTS $TABLE;
 CREATE TABLE $TABLE ($FIELDS);
 
