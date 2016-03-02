0c4c3657dbc9e7cbe8f7710cb41757c060a199ee
Brad Foster <bradf@121customerinsight.co.uk>
2016-03-02 11:29:05 +0000

    added dynamic panels

---
+++ application/views/records/panels/custom_panels.php
@@ -0,0 +1,9 @@
+ <div class="panel panel-primary custom-panel" custom-panel-display="<?php echo $display ?>" custom-panel-id="<?php echo $custom_panel_id ?>">
+      <div class="panel-heading clearfix"><span class="title"><?php echo $name ?></span>
+      <span class='btn btn-default btn-xs pull-right edit-custom-btn marl' style="display:none" custom-data-id=''><span class='glyphicon glyphicon-pencil'></span> Edit</span>
+	  <span class="btn btn-default btn-xs pull-right add-custom-btn marl" custom-panel-id="<?php echo $custom_panel_id ?>"><span class="glyphicon glyphicon-plus"></span> New</span>
+	 </div>
+      <div class="panel-body" style="overflow-x:auto">
+          <img src="<?php echo base_url(); ?>assets/img/ajax-loader-bar.gif" />
+      </div>
+    </div>
---
+++ assets/js/custom_panels.js
@@ -0,0 +1,267 @@
+var custom_panels = {
+    init: function() {
+        custom_panels.load_all_panels();
+        $(document).on('click', '.add-custom-btn', function() {
+            var id = $(this).attr('custom-panel-id');
+            custom_panels.load_form(id, false);
+        });
+        $(document).on('click', '.edit-custom-btn', function() {
+            var data_id = $(this).attr('custom-data-id');
+            var id = $(this).closest('.custom-panel').attr('custom-panel-id');
+            custom_panels.load_form(id, data_id);
+        });
+		$(document).on('click','[data-tab]',function(){
+			var data_id = $(this).attr('data-tab');
+			$(this).parent().find('[data-tab]').removeClass('text-primary');
+			$(this).addClass('text-primary');
+			$(this).closest('.panel-body').find('.table-wrapper').hide();
+			$(this).closest('.panel-body').find('.table-wrapper[data-list-id="'+data_id+'"]').show();
+			$(this).closest('.panel').find('.edit-custom-btn').attr('custom-data-id',data_id);
+		});
+        $modal.on('click', '#save-custom-panel', function() {
+            var data_id = $(this).attr('custom-data-id');
+            var panel_id = $(this).attr('custom-panel-id');
+            custom_panels.save_form(panel_id);
+        });
+
+
+        $modal.on('click', '.toggle-buttons button', function(e) {
+            e.preventDefault();
+            $(this).closest('.toggle-buttons').children().removeClass('active').removeClass('btn-primary').addClass('btn-default');
+            $(this).addClass('active').addClass('btn-primary').removeClass('btn-default');
+            $(this).closest('.toggle-buttons').next('input').val($(this).text());
+        });
+
+    },
+    load_all_panels: function() {
+        $('.custom-panel').each(function() {
+            var id = $(this).attr('custom-panel-id');
+            var display = $(this).attr('custom-panel-display');
+            custom_panels.load_panel(id, display, false);
+        });
+    },
+    load_panel: function(panel_id, display, data_id) {
+        $.ajax({
+            url: helper.baseUrl + 'ajax/load_custom_panel',
+            data: {
+                urn: record.urn,
+                id: panel_id
+            },
+            dataType: "JSON",
+            type: "POST",
+            beforeSend: function() {
+                $('.custom-panel[custom-panel-id="' + panel_id + '"]').find('.panel-body').html('<img src="' + helper.baseUrl + 'assets/img/ajax-loader-bar.gif" /> ');
+            }
+        }).done(function(response) {
+            if (display == "table") {
+                var html = custom_panels.load_table(response);
+            } else {
+                var html = custom_panels.load_list(response,data_id);
+				 $('.custom-panel[custom-panel-id="' + panel_id + '"]').find('.edit-custom-btn').show().attr('custom-data-id',Object.keys(response.data)[0]);
+            }
+            $('.custom-panel[custom-panel-id="' + panel_id + '"]').find('.panel-body').html(html);
+			
+        });
+    },
+    load_table: function(response) {
+        var html = "";
+        var blanks = "";
+        html = "<table class='table "+response.panel.table_class+"'><thead><tr>";
+        $.each(response.fields, function(id, field) {
+            html += "<th>" + field.name + "</th>";
+            blanks += "<td>-</td>";
+        });
+        html += "<th></th>";
+        html += "</tr></thead><tbody>";
+        if ($.isEmptyObject(response.data)) {
+            html += "<tr>" + blanks + "</tr>";
+        } else {
+            $.each(response.data, function(id, row) {
+                html += "<tr>";
+                $.each(response.fields, function(field_id, field) {
+                    if (typeof row[field_id] !== "undefined") {
+                        var value = row[field_id].value;
+                    } else {
+                        var value = "-";
+                    }
+                    if (field.type == "checkbox" && value == "on") {
+                        value = "<span class='fa fa-check'></span>"
+                    }
+                    html += "<td>" + value + "</td>";
+                });
+                html += "<td><span class='btn btn-default btn-xs pull-right edit-custom-btn marl' custom-data-id='" + id + "'><span class='glyphicon glyphicon-pencil'></span> Edit</span></td>";
+                html += "</tr>";
+            });
+        }
+
+        html += "</tbody></table>";
+        return html;
+    },
+    load_list: function(response,visible_data_set) {
+        var html = "";
+		 var wrapper = "";
+		var table = "";
+        var header = "";
+		var meta = "";
+		var list_counts = "<div class='pull-right'>";
+        if ($.isEmptyObject(response.data)) {
+            table = "<table class='"+response.data.table_class+"'>";
+            $.each(response.fields, function(id, field) {
+                table += "<tr><th>" + field.name + "</th><td>-</td></tr>";
+            });
+            table += "</table>";
+        } else {
+			var i=1; var last_set = Object.keys(response.data).length;
+            $.each(response.data, function(data_id, row) {
+				var hidden = "style='display:none'"; var color=""; var table = "";
+				if(data_id==visible_data_set){
+				hidden = ""; color="text-primary";
+				} else if(i==last_set&&!visible_data_set){ 
+				hidden = ""; color="text-primary";
+				}
+				wrapper = "<div class='table-wrapper' "+hidden+" data-list-id='"+data_id+"'>";
+				list_counts += "<span class='fa fa-circle pointer "+color+"' data-tab='"+data_id+"'></span> ";
+                table += "<table data-list-id='"+data_id+"' class='table "+response.panel.table_class+"'>";		
+				
+                $.each(response.fields, function(id, field) {
+                   if(typeof response.data[data_id][id] !== "undefined"){ var val=response.data[data_id][id].value
+				   meta = "<span style='padding-bottom:5px' class='pull-left small'>Added on "+response.data[data_id][id].created_on+"</span>";
+				    } else { var val= "-"; }
+                    if (field.type == "checkbox" && val == "on") {
+                        val = "<span class='fa fa-check'></span>"
+                    }
+                    table += "<tr><th style='white-space:nowrap;padding-right:5px'>" + field.name + "</th><td style='width:100%'>" + val + "</td></tr>";
+                });
+                table += "</table>";
+				html += wrapper+meta+table+"</div>";
+				i++;
+            });
+			list_counts += "</div>";
+			if(i>1){
+			html = list_counts+html;
+			}
+        }
+	
+        return html;
+    },
+    load_form: function(panel_id, data_id) {
+        $.ajax({
+            url: helper.baseUrl + 'ajax/load_custom_form',
+            data: {
+                urn: record.urn,
+                id: panel_id
+            },
+            dataType: "JSON",
+            type: "POST"
+        }).done(function(response) {
+            html = "<form><div class='row'>";
+            html += "<input type='hidden' name='urn' value='" + record.urn + "'/>";
+            if (data_id) {
+                html += "<input type='hidden' name='data_id' value='" + data_id + "'/>";
+            } else {
+                html += "<input type='hidden' name='data_id'/>";
+            }
+            $.each(response.fields, function(i, column) {
+                html += "<div class='col-sm-6'>";
+                $.each(column, function(field_id, field) {
+                    html += "<div class='form-group'>";
+                    html += "<label>" + field.name + "</label>"
+                    if (field.tooltip.length > 0) {
+                        html += "<span class='pointer glyphicon glyphicon-info-sign marl' data-toggle='tooltip' title='" + field.tooltip + "'></span>";
+                    }
+                    html += "<br>";
+                    if (data_id && typeof response.data[data_id][i][field_id] !== "undefined") {
+                        var value = response.data[data_id][i][field_id]['value']
+                    } else {
+                        var value = "";
+                    }
+
+                    if (field.type == "string") {
+                        html += "<input type='text' name='" + field.field_id + "' class='form-control input-sm' value='" + value + "'/>";
+                    }
+                    if (field.type == "number" || field.type == "decimal") {
+                        html += "<input type='text' name='" + field.field_id + "' class='number form-control input-sm' value='" + value + "'/>";
+                    }
+                    if (field.type == "date") {
+                        html += "<input type='text' name='" + field.field_id + "' class='form-control date input-sm' value='" + value + "'/>";
+                    }
+                    if (field.type == "datetime") {
+                        html += "<input type='text' name='" + field.field_id + "' class='form-control datetime input-sm' value='" + value + "'/>";
+                    }
+                    if (field.type == "select") {
+                        html += "<select name='" + field.field_id + "' class='selectpicker'><option value=''>--Please select--</option>";
+                        $.each(field.options, function(o, option) {
+                            var selected = "";
+                            if (value == option.option_id) {
+                                selected = "selected";
+                            }
+                            html += "<option " + selected + " value='" + option.option_id + "'>" + option.option_name + "</option>";
+                        });
+                        html += "</select>";
+                    }
+                    if (field.type == "multiple") {
+                        html += "<select name='" + field.field_id + "[]' multiple class='selectpicker'>";
+                        $.each(field.options, function(o, option) {
+                            var selected = "";
+                            //dirty hack to check if the option value is in the comma seperated list
+                            if (value.split(',').indexOf(option.option_id) > -1) {
+                                selected = "selected";
+                            }
+                            html += "<option " + selected + " value='" + option.option_id + "'>" + option.option_name + "</option>";
+                        });
+                        html += "</select>";
+                    }
+                    if (field.type == "buttons") {
+                        html += "<div class='btn-group toggle-buttons' role='group' >";
+                        $.each(field.options, function(o, option) {
+                            html += '<button class="btn btn-default">' + option.option_name + '</button>';
+                        });
+                        html += "</div>";
+                    }
+                    if (field.type == "checkbox") {
+                        var checked = "";
+                        if (value == "on") {
+                            checked = "checked";
+                        }
+                        html += "<input " + checked + " type='checkbox' name='" + field.field_id + "' class='input-sm'/>";
+                    }
+                    html += "</div>";
+                });
+                html += "</div>";
+            });
+            html += "</div></form>";
+            var mheader = $('.custom-panel[custom-panel-id="' + panel_id + '"]').find('span.title').text();
+            var mfooter = '<button type="submit" class="btn btn-primary pull-right" id="save-custom-panel" custom-panel-id="' + panel_id + '">Save</button> <button data-dismiss="modal" class="btn btn-default close-modal pull-left" type="button">Cancel</button>';
+			//load the modal
+            modals.load_modal(mheader, html, mfooter);
+            modal_body.css('overflow', 'visible');
+			//initialise the datepickers
+            $modal.find('.date').datetimepicker({
+                format: 'DD/MM/YYYY',
+                showClear: true
+            });
+            $modal.find('.datetime').datetimepicker({
+                format: 'DD/MM/YYYY HH:mm',
+                showClear: true,
+                sideBySide: true
+            });
+			//initialize any tooltips
+            $modal.find('[data-toggle="tooltip"]').tooltip();
+        });
+    },
+    save_form: function(panel_id) {
+        $.ajax({
+            url: helper.baseUrl + 'ajax/save_custom_panel',
+            data: $modal.find('form').serialize(),
+            dataType: "JSON",
+            type: "POST"
+        }).done(function(response) {
+            var display = $('.custom-panel[custom-panel-id="' + panel_id + '"]').attr('custom-panel-display');
+            //reload the panel with the updated values
+            custom_panels.load_panel(panel_id, display, response.data_id);
+            //close the modal
+            $modal.modal('toggle');
+        });
+    },
+
+}
