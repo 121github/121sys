7bc88038e325cfcb9806476c1d937e20b64cff30
Brad Foster <bradf@121customerinsight.co.uk>
2016-03-07 11:34:58 +0000

    added custom fields to modals

---
+++ application/migrations/104_update_104.php
@@ -0,0 +1,25 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_update_104 extends CI_Migration
+{
+
+    public function __construct()
+    {
+        $this->load->model('Database_model');
+    }
+
+    public function up()
+    {
+				 $check = $this->db->query("SHOW COLUMNS FROM `custom_panel_fields` LIKE 'sort'");
+        if(!$check->num_rows()){
+	$this->db->query("ALTER TABLE `custom_panel_fields` ADD `sort` INT( 5 ) NOT NULL");	
+		}
+	}
+	
+	public function down()
+    {
+		
+	}
+
+}
