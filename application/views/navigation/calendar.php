6626d541a8b2d310eac4f1d3e7adc82500cbcd5a
Brad Foster <bradf@121customerinsight.co.uk>
2016-04-04 12:20:41 +0100

    new calendar

--- application/views/navigation/calendar.php
+++ application/views/navigation/calendar.php
@@ -1,5 +1,5 @@
 <?php if (@in_array('full calendar', $_SESSION['permissions']) && @in_array('view appointments', $_SESSION['permissions'])) { ?>
                         <li <?php if ($this->uri->segment(1) == "calendar") {
                             echo "class='Selected'";
-                        } ?>><a href="<?php echo base_url(); ?>calendar">Calendar</a></li>
+                        } ?>><a href="<?php echo base_url(); ?>booking">Calendar</a></li>
                     <?php } ?>
