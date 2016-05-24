ac29173cfa5fdd48b859e316ffef1cd6ad9673a7
Brad Foster <bradf@121customerinsight.co.uk>
2016-05-24 16:53:26 +0100

    fixed alerts

--- application/views/user/restore_password.php
+++ application/views/user/restore_password.php
@@ -4,8 +4,8 @@
              <span style="color: red; font-size: 11px; display: none;" class="restore-password-error"></span>
              <input type="hidden" name="user_id" value="<?php echo $user_id; ?>">
              <input type="hidden" name="username" value="<?php echo $username; ?>">
-             <input type="password" name="new_pass" class="form-control" placeholder="New Password" <?php echo ($this->session->flashdata('username')?"autofocus":"") ?> required>
-             <input type="password" name="conf_pass" class="form-control" placeholder="Confirm Password" <?php echo ($this->session->flashdata('username')?"autofocus":"") ?> required>
+             <input type="password" name="new_pass" class="form-control" placeholder="New Password" <?php echo ($username?"autofocus":"") ?> required>
+             <input type="password" name="conf_pass" class="form-control" placeholder="Confirm Password" <?php echo ($username?"autofocus":"") ?> required>
         </div>
         <button class="btn btn-lg btn-primary save-restored-password" type="submit">Set as the new password</button>
 </form>
