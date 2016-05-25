dbb7b70b7f3409966d3e646310bac8eb142b333c
Brad Foster <bradf@121customerinsight.co.uk>
2016-05-25 16:39:11 +0100

    mig 140

--- application/migrations/140_update_140.php
+++ application/migrations/140_update_140.php
@@ -13,6 +13,9 @@ class Migration_update_140 extends CI_Migration
     {
 $this->db->query("INSERT ignore INTO `appointment_types` (`appointment_type_id`, `appointment_type`, `is_default`, `icon`) VALUES
 ('', 'Imported', 0, 'fa fa-google')");
+$this->db->query("INSERT ignore INTO `datafields` (`datafield_id`, `datafield_title`, `datafield_alias`, `datafield_select`, `datafield_order`, `datafield_group`, `datafield_table`, `campaign`, `modal_keys`) VALUES (NULL, 'Start', 'start', 'date_format(a.start,''%d/%m/%Y %H:%i'')', 'date_format(a.start,'%a %D %b %Y %l%:%i%p')', 'Appointments', 'appointments', NULL, '1')");
+$this->db->query("update modal_datafields set datafield_id = (select datafield_id from datafields where datafield_title='Start') where datafield_id = (select datafield_id from datafields where datafield_title='Start Date')");
+
 	}
 	
 }
