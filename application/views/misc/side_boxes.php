75288f1d264731034e2cfb9801a00fbdc6c16bb5
Brad Foster <bradf@121customerinsight.co.uk>
2016-06-10 14:22:18 +0100

    js minify

---
+++ application/views/misc/side_boxes.php
@@ -0,0 +1,8 @@
+<div id="quick-actions-box">
+    <a href="#quick-actions-right">
+        <span class="fa fa-caret-left quick-actions-btn"></span>
+    </a>
+</div>
+<div id="color-box">
+        <a href="#"><span class="glyphicon glyphicon-cog color-btn"></span></a>
+</div>
